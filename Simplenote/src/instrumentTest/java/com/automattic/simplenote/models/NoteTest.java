68b6c85ac8614e45b7df47b8ba7193e550602fd7
Beau Collins <robert@automattic.com>
2013-09-18 16:35:44 -0700

    Fixing note content preview unit test

--- Simplenote/src/instrumentTest/java/com/automattic/simplenote/models/NoteTest.java
+++ Simplenote/src/instrumentTest/java/com/automattic/simplenote/models/NoteTest.java
@@ -48,7 +48,8 @@ public class NoteTest extends TestCase {
         String title = "Lorem ipsum dolor sit amet,";
         String preview = "consectetur adipisicing elit, "
             + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-            + "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+            + "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            + "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pa";
 
         mNote.setContent("Lorem ipsum dolor sit amet,\n"
             + "consectetur adipisicing elit,\n"
