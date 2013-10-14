3f29b32e2f6988e84b0ecbc754bd57e9afb0676f
Beau Collins <robert@automattic.com>
2013-10-14 08:40:58 -0700

    Snippet formatter tests

--- Simplenote/src/instrumentTest/java/com/automattic/simplenote/utils/SearchSnippetFormatterTest.java
+++ Simplenote/src/instrumentTest/java/com/automattic/simplenote/utils/SearchSnippetFormatterTest.java
@@ -2,15 +2,31 @@ package com.automattic.simplenote.utils;
 
 import junit.framework.TestCase;
 
-import android.text.SpannableString;
+import android.text.Spannable;
+
+import com.automattic.simplenote.utils.SearchSnippetFormatter.SpanFactory;
 
 public class SearchSnippetFormatterTest extends TestCase {
 
     public void testFormatSnippet(){
-        String snippet = "\u2026 This is just <match>an example</match> of a snippet \u2026";
-        SpannableString formatted = SearchSnippetFormatter.formatString(snippet);
+        String snippet = "\u2026 This is just <match>an example</match> of a\n<match>snippet</match> \u2026";
+        Spannable formatted = SearchSnippetFormatter.formatString(snippet, new SpanFactory() {
+            @Override
+            public Object[] buildSpans(String content) {
+                return new Object[]{ new Object() };
+            }
+        });
 
         assertEquals("\u2026 This is just an example of a snippet \u2026", formatted.toString());
+
+        Object[] spans = formatted.getSpans(0, formatted.length(), Object.class);
+        assertEquals(2, spans.length);
+
+        assertEquals(15, formatted.getSpanStart(spans[0]));
+        assertEquals(25, formatted.getSpanEnd(spans[0]));
+
+        assertEquals(31, formatted.getSpanStart(spans[1]));
+        assertEquals(38, formatted.getSpanEnd(spans[1]));
 
     }
 
