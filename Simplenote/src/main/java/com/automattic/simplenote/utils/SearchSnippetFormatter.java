571f88f88c4cab1a8b466279ebb7d82c747e9925
Beau Collins <robert@automattic.com>
2013-10-14 08:41:41 -0700

    updating snippet highlighter

--- Simplenote/src/main/java/com/automattic/simplenote/utils/SearchSnippetFormatter.java
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/SearchSnippetFormatter.java
@@ -1,40 +1,76 @@
 package com.automattic.simplenote.utils;
 
+import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 
 public class SearchSnippetFormatter {
+
+    public interface SpanFactory {
+        public Object[] buildSpans(String content);
+    }
 
     static public final char OPEN_BRACKET = '<';
     static public final String OPEN_MATCH = "<match>";
     static public final String CLOSE_MATCH = "</match>";
 
-    public static SpannableString formatString(String snippet) {
-        return (new SearchSnippetFormatter(snippet)).toSpannableString();
+    static public final int OPEN_MATCH_LENGTH = OPEN_MATCH.length();
+    static public final int CLOSE_MATCH_LENGTH = CLOSE_MATCH.length();
+
+    public static Spannable formatString(String snippet, SpanFactory factory) {
+        return (new SearchSnippetFormatter(factory, snippet)).toSpannableString();
     }
+
 
     private String mSnippet;
+    private SpanFactory mFactory;
 
-    public SearchSnippetFormatter(String snippet) {
-        mSnippet = snippet;
+    public SearchSnippetFormatter(SpanFactory factory, String text){
+        mSnippet = text;
+        mFactory = factory;
     }
 
-    private SpannableString parseSnippet(){
+    private Spannable parseSnippet(){
         SpannableStringBuilder builder = new SpannableStringBuilder();
-        int length = mSnippet.length();
+        String snippet = mSnippet.replace("\n", " ");
         boolean inMatch = false;
 
-        char current;
-        for (int position=0; position < length; position++) {
-            current = mSnippet.charAt(position);
+        int position = 0;
+        int lastOpen = -1;
 
-            if (current == OPEN_BRACKET) {
-                // check if we opened a match
+        do {
+            if (inMatch) {
+                int close = snippet.indexOf(CLOSE_MATCH, position);
+                if (close == -1){
+                    builder.append(snippet.substring(position));
+                    break;
+                }
+                String highlighted = snippet.substring(position, close);
+                int start = builder.length();
+                builder.append(highlighted);
+                int end = builder.length();
+                Object[] spans = mFactory.buildSpans(highlighted);
+                for (Object span : spans) {
+                    builder.setSpan(span, start, end, 0x0);
+                }
+                inMatch = false;
+                position = close + CLOSE_MATCH_LENGTH;
+            } else {
+                int open = snippet.indexOf(OPEN_MATCH, position);
+                if (open == -1) {
+                    builder.append(snippet.substring(position));
+                    break;
+                }
+                builder.append(snippet.substring(position, open));
+                inMatch = true;
+                position = open + OPEN_MATCH_LENGTH;
             }
+        } while(position > -1);
 
-        }
+        return builder;
     }
 
-    public SpannableString toSpannableString() {
+    public Spannable toSpannableString() {
         return parseSnippet();
     }
 
