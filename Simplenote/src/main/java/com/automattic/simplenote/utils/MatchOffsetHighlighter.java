547417fa11745e841e782b6df8257e4a0bd714db
Beau Collins <robert@automattic.com>
2013-10-15 11:29:40 -0700

    MatchOffsetHighlighter runs in background thread

--- Simplenote/src/main/java/com/automattic/simplenote/utils/MatchOffsetHighlighter.java
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/MatchOffsetHighlighter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import java.util.Scanner;
 import java.io.UnsupportedEncodingException;
 
-public class MatchOffsetHighlighter {
+public class MatchOffsetHighlighter implements Runnable {
 
     public interface SpanFactory {
         public Object[] buildSpans();
@@ -28,56 +28,72 @@ public class MatchOffsetHighlighter {
             Object[] spans = factory.buildSpans();
 
             for (Object span : spans) {
-                android.util.Log.d("Simplenote", "Setting span");
                 content.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             }
         }
 
     }
 
-    public static void highlightMatchesOnTextView(final TextView textView, final String matches,
-        final int columnIndex, final SpanFactory factory) {
+    private SpanFactory mFactory;
+    private Thread mThread;
+    private TextView mTextView;
+    private String mMatches;
+    private int mIndex;
+    private Spannable mText;
+    private boolean mStopped = false;
 
-        final Spannable content = textView.getEditableText();
-        final Handler handler = textView.getHandler();
-        final OnMatchListener listener = new DefaultMatcher();
+    protected static OnMatchListener sListener = new DefaultMatcher();
 
-        Thread highlighter = new Thread(new Runnable() {
+    private OnMatchListener mListener = new OnMatchListener() {
 
-            @Override
-            public void run(){
-                highlightMatches(content, matches, columnIndex, factory, new OnMatchListener() {
+        @Override
+        public void onMatch(final SpanFactory factory, final Spannable text, final int start, final int end) {
 
-                    @Override
-                    public void onMatch(final SpanFactory factory, final Spannable content,
-                        final int start, final int end) {
+            mTextView.getHandler().post(new Runnable() {
 
-                        if (textView == null){
-                            Thread.currentThread().interrupt();
-                            return;
-                        }
+                @Override
+                public void run(){
+                    if (mStopped) return;
+                    sListener.onMatch(factory, text, start, end);
+                }
 
-                        if (handler == null) {
-                            Thread.currentThread().interrupt();
-                            return;
-                        }
+            });
+        }
 
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run(){
-                                if (textView != null)
-                                    listener.onMatch(factory, content, start, end);
-                            }
-                        });
+    };
 
-                    }
-                });
-                android.util.Log.d("Simplenote", "Done highlighting");
-            }
+    public MatchOffsetHighlighter(SpanFactory factory, TextView textView) {
+        mFactory = factory;
+        mTextView = textView;
+    }
 
-        });
+    @Override
+    public void run(){
+        highlightMatches(mText, mMatches, mIndex, mFactory, mListener);
+    }
 
-        highlighter.start();
+    public void start(){
+        // if there are no matches, we don't have to do anything
+        if (TextUtils.isEmpty(mMatches)) return;
+
+        mThread = new Thread(this);
+        mStopped = false;
+        mThread.start();
+    }
+
+    public void stop(){
+        mStopped = true;
+        if (mThread != null) mThread.interrupt();
+    }
+
+    public void highlightMatches(String matches, int columnIndex) {
+        synchronized(this){
+            stop();
+            mText = mTextView.getEditableText();
+            mMatches = matches;
+            mIndex = columnIndex;
+            start();
+        }
     }
 
     public static void highlightMatches(Spannable content, String matches, int columnIndex,
@@ -96,11 +112,10 @@ public class MatchOffsetHighlighter {
 
         int total_offset = 0;
 
+        // TODO: keep track of offsets and last index so we don't have to recalculate the entire byte length for every match which is pretty memory intensive
         while (scanner.hasNext()) {
 
             if (Thread.interrupted()) return;
-
-            android.util.Log.d("Simplenote", "adding highlight");
 
             int column = scanner.nextInt();
             int token = scanner.nextInt();
@@ -120,8 +135,17 @@ public class MatchOffsetHighlighter {
 
     static public final String CHARSET = "UTF-8";
 
+    // TODO: get ride of memory pressure by preventing the toString()
     protected static int getByteOffset(CharSequence text, int start, int end) {
+        // int byteLength = 0, charLength = end - start, char c;
+        // for (int i = start; i<end; i++) {
+        //     c = text.charAt(i);
+        // }
+        // char[] value = text.subSequence(start, end);
+        // byte[] bytes = Charsets.toUtf8Bytes(value, offset, count)
+        // char[] value = TextUtils.getChars();
         String substring = text.toString().substring(start, end);
+        // CharSequence sub = text.subSequence(start, end);
         try {
             return substring.length() - substring.getBytes(CHARSET).length;
         } catch (UnsupportedEncodingException e) {
