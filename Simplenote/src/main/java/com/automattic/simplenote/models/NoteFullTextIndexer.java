ca4f63309865a83f1b5484daaef82e6a12d18756
Beau Collins <robert@automattic.com>
2013-10-22 22:58:34 -0700

    Simperium library compatibility

--- Simplenote/src/main/java/com/automattic/simplenote/models/NoteFullTextIndexer.java
+++ Simplenote/src/main/java/com/automattic/simplenote/models/NoteFullTextIndexer.java
@@ -1,8 +1,9 @@
 package com.automattic.simplenote.models;
 
-import android.content.ContentValues;
-
 import com.simperium.client.FullTextIndex;
+
+import java.util.Map;
+import java.util.HashMap;
 
 public class NoteFullTextIndexer implements FullTextIndex.Indexer<Note> {
 
@@ -10,8 +11,8 @@ public class NoteFullTextIndexer implements FullTextIndex.Indexer<Note> {
     static public final String[] INDEXES = Note.FULL_TEXT_INDEXES;
 
     @Override
-    public ContentValues index(String[] keys, Note note){
-        ContentValues values = new ContentValues(keys.length);
+    public Map<String,String> index(String[] keys, Note note){
+        Map<String,String> values = new HashMap<String,String>(keys.length);
         values.put(INDEXES[0], note.getTitle());
         values.put(INDEXES[1], note.getContent());
         return values;
