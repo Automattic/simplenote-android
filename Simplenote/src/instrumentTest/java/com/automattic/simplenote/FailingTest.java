bb9c21c43bee2087ab8a8c45684c13259e56a03e
Robert "Beau" Collins <robert@automattic.com>
2013-11-02 10:08:54 -0700

    Update FailingTest.java

--- Simplenote/src/instrumentTest/java/com/automattic/simplenote/FailingTest.java
+++ Simplenote/src/instrumentTest/java/com/automattic/simplenote/FailingTest.java
@@ -6,6 +6,8 @@ public class FailingTest extends TestCase {
 
     /**
      * This test should fail the build
+     * 
+     * Unfortunately the emulator doesn't run very well
      */
     public void testThatFails() throws Exception {
         fail("Ha! It Failed!");
