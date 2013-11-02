690252ce145112db9e849bae6c8df2b18a98efc7
Beau Collins <robert@automattic.com>
2013-11-02 09:53:43 -0700

    Triggering build server
    hopefully

--- Simplenote/src/instrumentTest/java/com/automattic/simplenote/FailingTest.java
+++ Simplenote/src/instrumentTest/java/com/automattic/simplenote/FailingTest.java
@@ -4,6 +4,9 @@ import junit.framework.TestCase;
 
 public class FailingTest extends TestCase {
 
+    /**
+     * This test should fail the build
+     */
     public void testThatFails() throws Exception {
         fail("Ha! It Failed!");
     }
