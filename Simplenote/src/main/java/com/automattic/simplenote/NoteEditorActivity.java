0c9f04aa170e900016ae83adb5de409ca2714d77
Tyler Heck <theck13@hotmail.com>
2016-03-09 10:53:31 -0700

    Add action icon animation to Note Editor

--- Simplenote/src/main/java/com/automattic/simplenote/NoteEditorActivity.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NoteEditorActivity.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 
 public class NoteEditorActivity extends AppCompatActivity {
     private TabLayout mTabLayout;
+    private Toolbar mToolbar;
     private NoteEditorViewPager mViewPager;
 
     @Override
@@ -32,8 +33,8 @@ public class NoteEditorActivity extends AppCompatActivity {
         // No title, please.
         setTitle("");
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
@@ -68,6 +69,26 @@ public class NoteEditorActivity extends AppCompatActivity {
             );
             mViewPager.setAdapter(noteEditorFragmentStatePagerAdapter);
             mViewPager.setPagingEnabled(false);
+            mViewPager.addOnPageChangeListener(
+                new NoteEditorViewPager.OnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        if (position == 1) {
+                            animateActionsExit();
+                        } else {
+                            animateActionsEnter();
+                        }
+                    }
+
+                    @Override
+                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                    }
+
+                    @Override
+                    public void onPageScrollStateChanged(int state) {
+                    }
+                }
+            );
 
             mTabLayout = (TabLayout) findViewById(R.id.tabs);
             mTabLayout.setupWithViewPager(mViewPager);
@@ -81,6 +102,36 @@ public class NoteEditorActivity extends AppCompatActivity {
         }
 
         super.onPause();
+    }
+
+    private void animateActionsEnter() {
+        int duration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        View view;
+
+        for (int i = 1; i < mToolbar.getChildCount(); i++) {
+            view = mToolbar.getChildAt(i);
+            view.setTranslationY(-400);
+            view.setAlpha(1);
+            view.animate()
+                    .setStartDelay(duration * (i + 1))
+                    .setDuration(duration * (i + 1))
+                    .translationY(0);
+            view.clearAnimation();
+        }
+    }
+
+    private void animateActionsExit() {
+        int duration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        View view;
+
+        for (int i = 1; i < mToolbar.getChildCount(); i++) {
+            view = mToolbar.getChildAt(i);
+            view.animate()
+                    .setDuration(duration * (i + 1))
+                    .translationY(-400)
+                    .alpha(0);
+            view.clearAnimation();
+        }
     }
 
     public void hideTabs() {
--- Simplenote/src/main/java/com/automattic/simplenote/NoteMarkdownFragment.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NoteMarkdownFragment.java
@@ -3,12 +3,29 @@ package com.automattic.simplenote;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class NoteMarkdownFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_note_markdown, container, false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (!isAdded()) {
+                    return false;
+                }
+
+                getActivity().finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
