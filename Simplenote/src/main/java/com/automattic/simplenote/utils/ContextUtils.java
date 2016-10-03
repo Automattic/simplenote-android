316f6c5d3765b3731e54ae030b2d5bcfa0caad39
Tyler Heck <theck13@users.noreply.github.com>
2016-10-03 11:39:49 -0600

    Add font size variables to css for markdown (#388)

---
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/ContextUtils.java
@@ -0,0 +1,45 @@
+package com.automattic.simplenote.utils;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ContextUtils {
+    public static String readCssFile(Context context, String css) {
+        InputStream stream = null;
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        String line;
+
+        try {
+            stream = context.getResources().getAssets().open(css);
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append('\n');
+            }
+
+            return builder.toString();
+        } catch (IOException ex) {
+            return null;
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (IOException ignored) {
+            }
+
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException ignored) {
+            }
+        }
+    }
+}
