a16a5c51d0e4ea3bcb68b27779b6b112734dcb96
Asmadek <alma2610@ya.ru>
2016-10-06 01:22:46 +0300

    * Calendar repair

--- Simplenote/src/main/java/com/automattic/simplenote/NoteEditorFragment.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NoteEditorFragment.java
@@ -1315,7 +1315,7 @@ public class NoteEditorFragment extends Fragment implements Bucket.Listener<Note
             onReminderUpdated(calendar);
         }
     }
-    public void updateReminder(GregorianCalendar aCalendar) {
+    public void updateReminder(Calendar aCalendar) {
         onReminderUpdated(aCalendar);
     }
 }
