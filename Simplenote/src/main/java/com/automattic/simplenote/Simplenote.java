077872a470c5c58c097a454194894e9c63d6f501
Dan Roundhill <dan@automattic.com>
2016-12-22 15:27:03 -0800

    Stop Buckets after App goes to Background (#414)
    * Update dependencies. Woo.
    
    * Stop the buckets after the app goes to the background for 10 seconds.
    
    * Fix compilation error with new vector drawable support dependency.
    
    * Version bump, too.
    
    * Revert "Fix compilation error with new vector drawable support dependency."
    
    This reverts commit d31eab2b86d437c29ab9266f79ad36f72a414479.
    
    * Revert "Update dependencies. Woo."
    
    This reverts commit 77e056d36b7ee675cbc02bfbcfd550e2453d4c11.
    
    * Once again updating to latest dependencies.
    
    * Fixed TextView vector drawables by wrapping them in a selector. See: https://medium.com/@chrisbanes/appcompat-v23-2-age-of-the-vectors-91cbafa87c88
    
    * Going back to older gradle version to fix travis

--- Simplenote/src/main/java/com/automattic/simplenote/Simplenote.java
+++ Simplenote/src/main/java/com/automattic/simplenote/Simplenote.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import android.content.ComponentCallbacks2;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.automattic.simplenote.analytics.AnalyticsTracker;
 import com.automattic.simplenote.analytics.AnalyticsTrackerGoogleAnalytics;
@@ -21,6 +23,8 @@ import com.simperium.client.BucketNameInvalid;
 import org.wordpress.passcodelock.AppLockManager;
 
 public class Simplenote extends Application {
+
+    private static final int TEN_SECONDS_MILLIS = 10000;
 
     // log tag
     public static final String TAG = "Simplenote";
@@ -59,6 +63,8 @@ public class Simplenote extends Application {
             throw new RuntimeException("Could not create bucket", e);
         }
 
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
         ApplicationLifecycleMonitor applicationLifecycleMonitor = new ApplicationLifecycleMonitor();
         registerComponentCallbacks(applicationLifecycleMonitor);
         registerActivityLifecycleCallbacks(applicationLifecycleMonitor);
@@ -93,9 +99,27 @@ public class Simplenote extends Application {
         // ComponentCallbacks
         @Override
         public void onTrimMemory(int level) {
-            // Send analytics if app is in the background
             if (level == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
                 mIsInBackground = true;
+
+                // Give the buckets some time to finish sync, then stop them
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!mIsInBackground) {
+                            return;
+                        }
+
+                        if (mNotesBucket != null) {
+                            mNotesBucket.stop();
+                        }
+                        if (mTagsBucket != null) {
+                            mTagsBucket.stop();
+                        }
+                    }
+                }, TEN_SECONDS_MILLIS);
+
+                // Send analytics if app is in the background
                 AnalyticsTracker.track(
                         AnalyticsTracker.Stat.APPLICATION_CLOSED,
                         AnalyticsTracker.CATEGORY_USER,
