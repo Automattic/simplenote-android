77c8ce6ebed77eaaa9f95d280cd7f20d1bb5e981
Dan Roundhill <dan@automattic.com>
2017-04-10 10:09:52 -0700

    Enforce Sign in but allow previous offline users to still use the app (#432)
    * Nuked the account lag.
    
    * Updates to force login for new users.
    
    * Cleanup.
    
    * Version bump.
    
    * Updating comment for PREF_ACCOUNT_REQUIRED to make more sense.

--- Simplenote/src/main/java/com/automattic/simplenote/NotesActivity.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NotesActivity.java
@@ -34,7 +34,6 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.automattic.simplenote.analytics.AnalyticsTracker;
 import com.automattic.simplenote.models.Note;
@@ -77,7 +76,6 @@ public class NotesActivity extends AppCompatActivity implements
     private String mTabletSearchQuery;
     private UndoBarController mUndoBarController;
     private View mFragmentsContainer;
-    private View mWelcomeView;
     private SearchView mSearchView;
     private MenuItem mSearchMenuItem;
     private NoteListFragment mNoteListFragment;
@@ -208,7 +206,6 @@ public class NotesActivity extends AppCompatActivity implements
         mNotesBucket.addOnDeleteObjectListener(this);
         mTagsBucket.addListener(mTagsMenuUpdater);
 
-        setWelcomeViewVisibility();
         updateNavigationDrawerItems();
 
         // if the user is not authenticated and the tag doesn't exist revert to default drawer selection
@@ -281,24 +278,6 @@ public class NotesActivity extends AppCompatActivity implements
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
         mDrawerList = (ListView) findViewById(R.id.drawer_list);
 
-        // Configure welcome view in the nav drawer
-        mWelcomeView = mDrawerLayout.findViewById(R.id.welcome_view);
-        TextView welcomeSignInButton = (TextView) mDrawerLayout.findViewById(R.id.welcome_sign_in_button);
-        welcomeSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startLoginActivity(true);
-            }
-        });
-
-        TextView welcomeCloseButton = (TextView) mDrawerLayout.findViewById(R.id.welcome_close);
-        welcomeCloseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                removeWelcomeView();
-            }
-        });
-
         View settingsButton = findViewById(R.id.nav_settings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -336,29 +315,6 @@ public class NotesActivity extends AppCompatActivity implements
         mDrawerLayout.addDrawerListener(mDrawerToggle);
     }
 
-    private void setWelcomeViewVisibility() {
-        if (mWelcomeView == null) return;
-        // Hide welcome view if user is signed in or closed the welcome view
-        if (userIsAuthorized() || PrefUtils.getBoolPref(this, PrefUtils.PREF_APP_TRIAL)) {
-            mWelcomeView.setVisibility(View.GONE);
-        } else {
-            mWelcomeView.setVisibility(View.VISIBLE);
-            mWelcomeView.setAlpha(1.0f);
-        }
-    }
-
-    private void removeWelcomeView() {
-        if (mWelcomeView == null) return;
-
-        AniUtils.swipeOutToLeft(mWelcomeView);
-
-        // Set preference so the welcome view never shows again
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean(PrefUtils.PREF_APP_TRIAL, true);
-        editor.apply();
-    }
-
     private void checkForFirstLaunch() {
         if (PrefUtils.getBoolPref(this, PrefUtils.PREF_FIRST_LAUNCH, true)) {
             // Create the welcome note
@@ -376,6 +332,7 @@ public class NotesActivity extends AppCompatActivity implements
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
             SharedPreferences.Editor editor = preferences.edit();
             editor.putBoolean(PrefUtils.PREF_FIRST_LAUNCH, false);
+            editor.putBoolean(PrefUtils.PREF_ACCOUNT_REQUIRED, true);
             editor.apply();
         }
     }
@@ -468,8 +425,13 @@ public class NotesActivity extends AppCompatActivity implements
         fm.executePendingTransactions();
     }
 
+    private boolean userAccountRequired() {
+        return PrefUtils.getBoolPref(this, PrefUtils.PREF_ACCOUNT_REQUIRED, false);
+    }
+
     /**
      * Checks for a previously valid user that is now not authenticated
+     * Also checks if user account is required (added in version 1.5.6)
      *
      * @return true if user has invalid authorization
      */
@@ -477,17 +439,14 @@ public class NotesActivity extends AppCompatActivity implements
         Simplenote currentApp = (Simplenote) getApplication();
         Simperium simperium = currentApp.getSimperium();
         User user = simperium.getUser();
-        return user.hasAccessToken() && user.getStatus().equals(User.Status.NOT_AUTHORIZED);
+        boolean isNotAuthorized = user.getStatus().equals(User.Status.NOT_AUTHORIZED);
+        return (user.hasAccessToken() && isNotAuthorized) ||
+                (userAccountRequired() && isNotAuthorized);
     }
 
     private boolean userIsUnauthorized() {
         Simplenote currentApp = (Simplenote) getApplication();
         return currentApp.getSimperium().getUser().getStatus() == User.Status.NOT_AUTHORIZED;
-    }
-
-    private boolean userIsAuthorized() {
-        Simplenote app = (Simplenote) getApplication();
-        return !app.getSimperium().needsAuthorization();
     }
 
     public void setCurrentNote(Note note) {
