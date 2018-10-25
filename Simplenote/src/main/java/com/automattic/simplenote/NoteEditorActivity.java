2cd6ae141a523f1de8d36f1f5f1aa837143c49c8
Roberto Orozco <issc.roberto@outlook.com>
2018-10-25 09:19:15 -0500

    Prevent IndexOutOfBoundsException by validating size of adapter

--- Simplenote/src/main/java/com/automattic/simplenote/NoteEditorActivity.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NoteEditorActivity.java
@@ -56,7 +56,7 @@ public class NoteEditorActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         mNoteId = intent.getStringExtra(NoteEditorFragment.ARG_ITEM_ID);
-        
+
         if (savedInstanceState == null) {
             // Create the note editor fragment
             Bundle arguments = new Bundle();
@@ -137,11 +137,11 @@ public class NoteEditorActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        if(mNoteEditorFragmentPagerAdapter.getItem(0).isAdded()) {
+        if (mNoteEditorFragmentPagerAdapter.getCount() > 0 && mNoteEditorFragmentPagerAdapter.getItem(0).isAdded()) {
             getSupportFragmentManager()
                     .putFragment(outState, getString(R.string.tab_edit), mNoteEditorFragmentPagerAdapter.getItem(0));
         }
-        if(mNoteEditorFragmentPagerAdapter.getItem(1).isAdded()) {
+        if (mNoteEditorFragmentPagerAdapter.getCount() > 1 && mNoteEditorFragmentPagerAdapter.getItem(1).isAdded()) {
             getSupportFragmentManager()
                     .putFragment(outState, getString(R.string.tab_preview), mNoteEditorFragmentPagerAdapter.getItem(1));
         }
