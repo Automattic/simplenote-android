874aa1b04cb8cecc66d00abc29056deffe6b1166
Dan Roundhill <dan@a8c.com>
2019-02-01 10:21:33 -0800

    Adding solution for checkboxes not causing keyboard to show if the EditText doesn't have focus. Props @theck13

--- Simplenote/src/main/java/com/automattic/simplenote/utils/SimplenoteMovementMethod.java
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/SimplenoteMovementMethod.java
@@ -22,37 +22,44 @@ public class SimplenoteMovementMethod extends ArrowKeyMovementMethod {
 
     @Override
     public boolean onTouchEvent(TextView textView, Spannable buffer, MotionEvent event) {
-        int action = event.getAction();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
 
-        if (action == MotionEvent.ACTION_UP) {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
+        x -= textView.getTotalPaddingLeft();
+        y -= textView.getTotalPaddingTop();
 
-            x -= textView.getTotalPaddingLeft();
-            y -= textView.getTotalPaddingTop();
+        x += textView.getScrollX();
+        y += textView.getScrollY();
 
-            x += textView.getScrollX();
-            y += textView.getScrollY();
+        Layout layout = textView.getLayout();
+        int line = layout.getLineForVertical(y);
+        int off = layout.getOffsetForHorizontal(line, x);
+        int lineStart = layout.getLineStart(line);
 
-            Layout layout = textView.getLayout();
-            int line = layout.getLineForVertical(y);
-            int off = layout.getOffsetForHorizontal(line, x);
-            int lineStart = layout.getLineStart(line);
+        // Also toggle the checkbox if the user tapped the space next to the checkbox
+        if (off == lineStart + 1) {
+            off = lineStart;
+        }
 
-            // Also toggle the checkbox if the user tapped the space next to the checkbox
-            if (off == lineStart + 1) {
-                off = lineStart;
+        CheckableSpan[] checkableSpans = buffer.getSpans(off, off + 1, CheckableSpan.class);
+
+        if (checkableSpans.length != 0) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (!textView.hasFocus()) {
+                        textView.setFocusableInTouchMode(false);
+                    }
+
+                    break;
+                case MotionEvent.ACTION_UP:
+                    checkableSpans[0].onClick(textView);
+                    textView.setFocusableInTouchMode(true);
+                    break;
             }
 
-            CheckableSpan[] checkableSpans = buffer.getSpans(off, off + 1, CheckableSpan.class);
-
-            if (checkableSpans.length != 0) {
-                checkableSpans[0].onClick(textView);
-
-                return true;
-            } else {
-                Selection.removeSelection(buffer);
-            }
+            return true;
+        } else {
+            Selection.removeSelection(buffer);
         }
 
         return false;
