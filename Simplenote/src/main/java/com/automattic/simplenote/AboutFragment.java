e9c089b53f2d4c5fa264ee61017eb4bd0d4e0273
Roberto Orozco <issc.roberto@outlook.com>
2019-02-28 13:49:48 -0600

    Add NonNull annotation to parameters to match parent method

--- Simplenote/src/main/java/com/automattic/simplenote/AboutFragment.java
+++ Simplenote/src/main/java/com/automattic/simplenote/AboutFragment.java
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -32,7 +33,7 @@ public class AboutFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_about, container, false);
 
