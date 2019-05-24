045d9314d24f3729c123ecfb3e5899e540486c72
Roberto Orozco <issc.roberto@outlook.com>
2019-05-24 12:33:50 -0500

    Fix Inspection Warnings (#648)
    * Use character class which is faster and simpler instead of char alternation
    
    * Add NonNull annotation to parameters to match parent method
    
    * Fix NullPointerException warnings
    
    * Make variables package-private to fix warnings

--- Simplenote/src/main/java/com/automattic/simplenote/AboutFragment.java
+++ Simplenote/src/main/java/com/automattic/simplenote/AboutFragment.java
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -32,7 +33,7 @@ public class AboutFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_about, container, false);
 
@@ -88,7 +89,7 @@ public class AboutFragment extends Fragment {
         playStore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse(PLAY_STORE_URI + getActivity().getPackageName());
+                Uri uri = Uri.parse(PLAY_STORE_URI + requireActivity().getPackageName());
                 Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
                 goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
                         Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
@@ -97,7 +98,7 @@ public class AboutFragment extends Fragment {
                     startActivity(goToMarket);
                 } catch (ActivityNotFoundException e) {
                     startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(PLAY_STORE_URL + getActivity().getPackageName())));
+                            Uri.parse(PLAY_STORE_URL + requireActivity().getPackageName())));
                 }
             }
         });
