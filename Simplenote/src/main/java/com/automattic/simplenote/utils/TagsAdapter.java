0c0bde7199968168aa2f04fefdcb5904491a6493
Tyler Heck <theck13@users.noreply.github.com>
2019-05-31 23:53:56 -0600

    Update layout attributes from left/right to start/end

--- Simplenote/src/main/java/com/automattic/simplenote/utils/TagsAdapter.java
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/TagsAdapter.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,6 @@ import com.automattic.simplenote.R;
 import com.automattic.simplenote.TagsActivity;
 import com.automattic.simplenote.models.Note;
 import com.automattic.simplenote.models.Tag;
-import com.automattic.simplenote.widgets.TintedTextView;
 import com.simperium.client.Bucket;
 import com.simperium.client.Query;
 
@@ -153,7 +153,7 @@ public class TagsAdapter extends BaseAdapter {
         }
         TagMenuItem tagMenuItem = getItem(position);
 
-        TintedTextView drawerItemText = view.findViewById(R.id.drawer_item_name);
+        AppCompatTextView drawerItemText = view.findViewById(R.id.drawer_item_name);
         drawerItemText.setText(tagMenuItem.name);
 
         int selectedPosition = ((ListView) viewGroup).getCheckedItemPosition() - mHeaderCount;
@@ -181,7 +181,11 @@ public class TagsAdapter extends BaseAdapter {
             dividerView.setVisibility(View.GONE);
         }
 
-        drawerItemText.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null, color);
+        if (icon != null) {
+            icon.setTint(color);
+        }
+
+        drawerItemText.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
         drawerItemText.setTextColor(color);
 
         View tagsHeader = view.findViewById(R.id.tags_header);
