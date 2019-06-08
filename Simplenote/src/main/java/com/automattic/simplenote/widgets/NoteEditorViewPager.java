3e5b568a9aa940afaec20344d3d762eba55c7b8e
Roberto Orozco <issc.roberto@outlook.com>
2019-06-08 00:08:54 -0500

    Remove unused import

--- Simplenote/src/main/java/com/automattic/simplenote/widgets/NoteEditorViewPager.java
+++ Simplenote/src/main/java/com/automattic/simplenote/widgets/NoteEditorViewPager.java
@@ -1,6 +1,5 @@
 package com.automattic.simplenote.widgets;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
