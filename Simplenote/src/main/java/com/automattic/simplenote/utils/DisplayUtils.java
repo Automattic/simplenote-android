e9a74a20c2d788c7fcaf43b7bbb4a8758170bef4
Tyler Heck <theck13@users.noreply.github.com>
2019-06-11 12:15:17 -0600

    Update indention from two to four spaces in display utils class

--- Simplenote/src/main/java/com/automattic/simplenote/utils/DisplayUtils.java
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/DisplayUtils.java
@@ -81,8 +81,8 @@ public class DisplayUtils {
         return DisplayUtils.dpToPx(context, PrefUtils.getFontSize(context)) + DisplayUtils.dpToPx(context, 6);
     }
 
-  // Disable screenshots if app PIN lock is on
-  public static void disableScreenshotsIfLocked(Activity activity) {
+    // Disable screenshots if app PIN lock is on
+    public static void disableScreenshotsIfLocked(Activity activity) {
         if (AppLockManager.getInstance().getAppLock().isPasswordLocked()) {
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         } else {
