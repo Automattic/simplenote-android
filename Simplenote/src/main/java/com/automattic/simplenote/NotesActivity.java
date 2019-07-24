c6b12ab4daf0145a57358028a2939516725198d8
Tyler Heck <theck13@users.noreply.github.com>
2019-07-24 15:30:54 -0600

    Update alert dialog from support to x library in notes activity class

--- Simplenote/src/main/java/com/automattic/simplenote/NotesActivity.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NotesActivity.java
@@ -1,7 +1,6 @@
 package com.automattic.simplenote;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -24,6 +23,7 @@ import android.widget.ProgressBar;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
