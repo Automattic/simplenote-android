215953ef439a7afa4dd156c864abe6c1a8c1680f
Tyler Heck <theck13@users.noreply.github.com>
2020-02-12 17:59:46 -0700

    Add note widget dark

--- Simplenote/src/main/java/com/automattic/simplenote/utils/WidgetUtils.java
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/WidgetUtils.java
@@ -5,11 +5,18 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
+import com.automattic.simplenote.NoteWidgetDark;
 import com.automattic.simplenote.NoteWidgetLight;
 
 public class WidgetUtils {
     public static void updateNoteWidgets(Context context) {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+        int[] idsDark = appWidgetManager.getAppWidgetIds(new ComponentName(context, NoteWidgetDark.class));
+        Intent updateIntentDark = new Intent();
+        updateIntentDark.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        updateIntentDark.putExtra(NoteWidgetDark.KEY_WIDGET_IDS_DARK, idsDark);
+        context.sendBroadcast(updateIntentDark);
 
         int[] idsLight = appWidgetManager.getAppWidgetIds(new ComponentName(context, NoteWidgetLight.class));
         Intent updateIntentLight = new Intent();
