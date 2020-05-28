48a483f683d3588abc43174900aafed7034bbf05
stavares <saratavares843@gmail.com>
2020-05-28 01:05:17 +0100

    add delete notes automated test flows

---
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/DeleteNotes.java
@@ -0,0 +1,55 @@
+package com.automattic.simplenote;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static com.automattic.simplenote.utils.EspressoUITests.addNote;
+import static com.automattic.simplenote.utils.EspressoUITests.deleteNoteFromList;
+import static com.automattic.simplenote.utils.EspressoUITests.logOut2;
+import static com.automattic.simplenote.utils.EspressoUITests.loginWithCredentials;
+import static com.automattic.simplenote.utils.EspressoUITests.loginWithValidCredentials;
+import static com.automattic.simplenote.utils.EspressoUITests.optionsTapTrash;
+import static com.automattic.simplenote.utils.EspressoUITests.tapNote;
+import static com.automattic.simplenote.utils.EspressoUITests.undoDeleteNoteFromList;
+
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class DeleteNotes {
+
+    @Rule
+    public ActivityTestRule<NotesActivity> mActivityTestRule = new ActivityTestRule<>(NotesActivity.class);
+
+    @Test
+    public void addNoteDeleteFromNotesList() throws InterruptedException {
+        loginWithCredentials();
+        loginWithValidCredentials();
+        addNote();
+        Thread.sleep(2000);
+        deleteNoteFromList();
+        undoDeleteNoteFromList();
+        deleteNoteFromList();
+        logOut2();
+    }
+
+    @Test
+    public void addNoteDeleteNoteFromDetail() throws InterruptedException {
+        loginWithCredentials();
+        loginWithValidCredentials();
+        addNote();
+        tapNote();
+        optionsTapTrash();
+        undoDeleteNoteFromList();
+        logOut2();
+    }
+
+    //TODO: Add a test flow emptying trash
+
+}
+
--- Simplenote/src/androidTest/java/com/automattic/simplenote/utils/EspressoUITests.java
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/utils/EspressoUITests.java
@@ -11,12 +11,23 @@ import com.automattic.simplenote.R;
 import org.junit.Rule;
 import org.junit.Test;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertContains;
 import static com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn;
 import static com.schibsted.spain.barista.interaction.BaristaDrawerInteractions.openDrawer;
 import static com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo;
+import static com.schibsted.spain.barista.interaction.BaristaKeyboardInteractions.closeKeyboard;
 import static com.schibsted.spain.barista.interaction.BaristaListInteractions.clickListItem;
 import static java.lang.Thread.sleep;
 
@@ -56,6 +67,147 @@ public class EspressoUITests {
         swipeToBottom();
         sleep(2000);
         clickOn(R.string.log_out);
+    }
+
+    @Test
+    public static void logOut2() throws InterruptedException {
+        openDrawer();
+        clickListItem(R.id.design_navigation_view, 3);
+        sleep(2000);
+        swipeToBottom();
+        sleep(2000);
+        clickOn(R.string.log_out);
+        onView(withId(android.R.id.button1)).perform((click()));
+    }
+
+    //TODO - Add a if/else logout test to migrate 2 logout's into one, to handle alertDialog when there's unsynced notes and user tries to logout
+
+    @Test
+    public static void tapNoteButton() {
+        clickOn(R.id.fab_button);
+    }
+
+    @Test
+    public static void addNoteContent() {
+        writeTo(R.id.note_content, "*-Re7]J4Ux8q)g?X");;
+
+    }
+
+    @Test
+    public static void addNote() {
+        tapNoteButton();
+        addNoteContent();
+        closeKeyboard();
+        Espresso.pressBack();
+        assertContains("*-Re7]J4Ux8q)g?X");
+    }
+
+    @Test
+    public static void deleteNoteFromList() {
+        onView(withText("*-Re7]J4Ux8q)g?X")).perform(longClick());
+        assertContains("1 note selected"); //using text instead of string because the string itself includes "%d note selected" and that's not the visible copy, the visible copy includes the notes number such as "1 note selected"
+        clickOn(R.id.menu_trash);
+        assertContains(R.string.note_deleted);
+    }
+
+    @Test
+    public static void undoDeleteNoteFromList() {
+        clickOn(R.string.undo);
+        assertContains("*-Re7]J4Ux8q)g?X");
+    }
+
+    @Test
+    public static void tapNote() {
+        clickOn("*-Re7]J4Ux8q)g?X");
+    }
+
+    @Test
+    public static void addChecklist() {
+        clickOn(R.id.menu_checklist);
+    }
+
+    @Test
+    public static void tapInformationButton() {
+        clickOn(R.id.menu_info);
+    }
+
+    @Test
+    public static void tapOutside() {
+        clickOn(R.id.touch_outside);
+    }
+
+    @Test
+    public static void moreOptions() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withId(R.string.more_options)).perform(click());
+    }
+
+    @Test
+    public static void optionsTapTrash() throws InterruptedException {
+        onView(withContentDescription(R.string.more_options))
+                .perform(click());
+        Thread.sleep(2000);
+        onView(withText("Trash")).perform(click()); //when using ID or position crashes on popup window menu test fails
+    }
+
+    @Test
+    public static void tapSearchNotes() {
+        clickOn(R.id.menu_search);
+    }
+
+    @Test
+    public static void performSearch() {
+        clickOn(R.id.search_src_text);
+    }
+
+    @Test
+    public static void tapPin() {
+        moreOptions();
+        clickOn(R.string.toggle_pin);
+    }
+
+    @Test
+    public static void tapMarkdown() {
+        moreOptions();
+        clickOn(R.id.markdown);
+    }
+
+    @Test
+    public static void tapShare() {
+        moreOptions();
+        clickOn(R.string.share);
+    }
+
+    @Test
+    public static void emptyHistory() {
+        moreOptions();
+        clickOn(R.string.history);
+        assertContains(R.string.error_history);
+    }
+
+    @Test
+    public static void tapTrashOptions() {
+        moreOptions();
+        clickOn(R.string.trash);
+    }
+
+    @Test
+    public static void trash() {
+        openDrawer();
+        clickListItem(R.id.design_navigation_view, 2);
+    }
+
+    @Test
+    public static void allNotes() {
+        openDrawer();
+        clickListItem(R.id.design_navigation_view, 1);
+    }
+
+    @Test
+    public static void emptyTrash() {
+        clickOn(R.id.menu_empty_trash);
+        onView(withId(android.R.id.button1)).perform((click()));
+        assertContains(R.string.empty_notes_trash);
     }
 
     public static void swipe(int start, int end, int delay) {
