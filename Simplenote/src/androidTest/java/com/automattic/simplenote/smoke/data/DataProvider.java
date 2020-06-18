36e7218e85fab836e2667d4c999fba95f4b39524
ahmet-izgi <ahmetizgi@gmail.com>
2020-06-18 23:06:10 +0300

    System.currentTimeMillis added to Note title to make it for accessible

--- Simplenote/src/androidTest/java/com/automattic/simplenote/smoke/data/DataProvider.java
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/smoke/data/DataProvider.java
@@ -13,9 +13,14 @@ public class DataProvider {
     public static List<NoteDTO> generateNotes(int size) {
         List<NoteDTO> noteDTOList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            NoteDTO note = new NoteDTO("Corona Days", "Corona Days... People need to support each other on these dooms days!", Arrays.asList("Corona"));
+            NoteDTO note = new NoteDTO("Corona Days " + getUniqueToken(), "Corona Days... People need to support each other on these dooms days!", Arrays.asList("Corona"));
             noteDTOList.add(note);
         }
         return noteDTOList;
+    }
+
+
+    private static String getUniqueToken() {
+        return String.valueOf(System.currentTimeMillis());
     }
 }
