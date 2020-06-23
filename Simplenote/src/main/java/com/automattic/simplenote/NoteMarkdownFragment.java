2027f365406496c434078002d265fdafc3f91ce6
Iskander Gaba <iskandergaba@gmail.com>
2020-06-23 15:41:24 +0200

    Updated a comment
    Co-authored-by: Tyler Heck <theck13@users.noreply.github.com>

--- Simplenote/src/main/java/com/automattic/simplenote/NoteMarkdownFragment.java
+++ Simplenote/src/main/java/com/automattic/simplenote/NoteMarkdownFragment.java
@@ -194,7 +194,7 @@ public class NoteMarkdownFragment extends Fragment implements Bucket.Listener<No
                 AndDown.HOEDOWN_HTML_ESCAPE
         );
 
-        // Set auto left or right alignment for lists, tables, and quotes based on language of start
+        // Set auto alignment for lists, tables, and quotes based on language of start.
         parsedMarkdown = parsedMarkdown
                 .replaceAll("<ol>", "<ol dir=\"auto\">")
                 .replaceAll("<ul>", "<ul dir=\"auto\">")
