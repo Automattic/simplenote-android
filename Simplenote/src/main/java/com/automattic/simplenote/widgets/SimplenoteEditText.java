9c40a2d0abb35561725089f730085dbe60e73614
Tyler Heck <theck13@users.noreply.github.com>
2020-08-03 15:58:30 -0600

    Add style font family attribute for monospace and serif fonts in styles

--- Simplenote/src/main/java/com/automattic/simplenote/widgets/SimplenoteEditText.java
+++ Simplenote/src/main/java/com/automattic/simplenote/widgets/SimplenoteEditText.java
@@ -35,19 +35,16 @@ public class SimplenoteEditText extends AppCompatEditText {
     public SimplenoteEditText(Context context) {
         super(context);
         listeners = new ArrayList<>();
-        setTypeface(TypefaceCache.getTypeface(context, TypefaceCache.TYPEFACE_NAME_ROBOTO_REGULAR));
     }
 
     public SimplenoteEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
         listeners = new ArrayList<>();
-        setTypeface(TypefaceCache.getTypeface(context, TypefaceCache.TYPEFACE_NAME_ROBOTO_REGULAR));
     }
 
     public SimplenoteEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         listeners = new ArrayList<>();
-        setTypeface(TypefaceCache.getTypeface(context, TypefaceCache.TYPEFACE_NAME_ROBOTO_REGULAR));
     }
 
     public void addOnSelectionChangedListener(OnSelectionChangedListener o) {
