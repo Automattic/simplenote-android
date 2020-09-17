d05263b7304c8b6fc55a585357c087d66f908510
Tyler Heck <theck13@users.noreply.github.com>
2020-09-17 09:08:12 -0600

    Add comment describing class and variables to reference class

--- Simplenote/src/main/java/com/automattic/simplenote/models/Reference.java
+++ Simplenote/src/main/java/com/automattic/simplenote/models/Reference.java
@@ -2,6 +2,14 @@ package com.automattic.simplenote.models;
 
 import java.util.Calendar;
 
+/**
+ * A note listed in the Information sheet which is an inbound reference to the open note.
+ *
+ * Calendar mDate   The last modified date of the note.
+ * String mKey      The Simperium key of the note.
+ * String mTitle    The title of the note.
+ * int mCount       The number of references to the open note in the referencing note.
+ */
 public class Reference {
     private Calendar mDate;
     private String mKey;
