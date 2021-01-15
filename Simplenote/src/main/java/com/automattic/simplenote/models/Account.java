77aa8c61e886b0a755985c288b8cc0a1c9b885ff
Tyler Heck <theck13@users.noreply.github.com>
2021-01-15 13:42:24 -0700

    Add account bucket class

---
+++ Simplenote/src/main/java/com/automattic/simplenote/models/Account.java
@@ -0,0 +1,60 @@
+package com.automattic.simplenote.models;
+
+import com.simperium.client.BucketObject;
+import com.simperium.client.BucketSchema;
+
+import org.json.JSONObject;
+
+public class Account extends BucketObject {
+    public static final String KEY_EMAIL_VERIFICATION = "email-verification";
+
+    private static final String BUCKET_NAME = "account";
+    private static final String FIELD_EMAIL_VERIFICATION_STATUS = "status";
+
+    public enum Status {
+        SENT,
+        VERIFIED
+    }
+
+    private Account(String key, JSONObject properties) {
+        super(key, properties);
+    }
+
+    public boolean hasConfirmedAccount() {
+        Object status = getProperty(FIELD_EMAIL_VERIFICATION_STATUS);
+
+        if (status == null) {
+            return false;
+        } else {
+            return Status.VERIFIED.toString().equalsIgnoreCase((String) status);
+        }
+    }
+
+    public boolean hasSentEmail() {
+        Object status = getProperty(FIELD_EMAIL_VERIFICATION_STATUS);
+
+        if (status == null) {
+            return false;
+        } else {
+            return Status.SENT.toString().equalsIgnoreCase((String) status);
+        }
+    }
+
+    public static class Schema extends BucketSchema<Account> {
+        public Schema() {
+            autoIndex();
+        }
+
+        public Account build(String key, JSONObject properties) {
+            return new Account(key, properties);
+        }
+
+        public String getRemoteName() {
+            return BUCKET_NAME;
+        }
+
+        public void update(Account account, JSONObject properties) {
+            account.setProperties(properties);
+        }
+    }
+}
