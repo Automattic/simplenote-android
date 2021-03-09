782a2bb83784b2b2995d12fb9a8c347cdc8db8fb
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-03-09 10:38:12 +0100

    Create a new signup activity

---
+++ Simplenote/src/main/java/com/automattic/simplenote/SimplenoteSignupActivity.java
@@ -0,0 +1,14 @@
+package com.automattic.simplenote;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimplenoteSignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
