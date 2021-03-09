09a3d6a692c5d6403c484becc8a1b35eacf8ecb5
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-03-09 17:24:36 +0200

    Init email confirmation text for the confirmation fragment

--- Simplenote/src/main/java/com/automattic/simplenote/authentication/ConfirmationFragment.java
+++ Simplenote/src/main/java/com/automattic/simplenote/authentication/ConfirmationFragment.java
@@ -1,21 +1,40 @@
 package com.automattic.simplenote.authentication;
 
 import android.os.Bundle;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.automattic.simplenote.R;
+import com.automattic.simplenote.utils.HtmlCompat;
 
 public class ConfirmationFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_confirmation, container, false);
+        View view = inflater.inflate(R.layout.fragment_confirmation, container, false);
+        initUi(view);
+        return view;
+    }
+
+    private void initUi(View view) {
+        initEmailConfirmation((TextView) view.findViewById(R.id.email_confirmation_text), "email");
+    }
+
+    private void initEmailConfirmation(TextView emailConfirmation, String email) {
+        String boldEmail = "<b>" + email + "</b>";
+        Spanned emailConfirmationText = HtmlCompat.fromHtml(String.format(
+            getString(R.string.email_confirmation_text),
+            boldEmail));
+        emailConfirmation.setText(emailConfirmationText);
+        emailConfirmation.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_80));
     }
 }
