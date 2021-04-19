2ff1c4a419ef1722c583451c36a1b9550bb54b7d
Danilo Dominguez <ddominguez@ubiqua.me>
2021-04-19 00:40:03 -0500

    Added rest of tests for AddTagActivity.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/BaseUITest.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/BaseUITest.kt
@@ -1,11 +1,43 @@
 package com.automattic.simplenote
 
 import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
+import com.automattic.simplenote.models.Tag
+import com.automattic.simplenote.utils.TagUtils
+import com.automattic.simplenote.utils.TestBucket
+import org.junit.Before
+import java.security.SecureRandom
 
 open class BaseUITest {
+    private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    private lateinit var application: SimplenoteTest
+    protected lateinit var tagsBucket: TestBucket<Tag>
+
+    @Before
+    fun setup() {
+        application = ApplicationProvider.getApplicationContext() as SimplenoteTest
+        tagsBucket = application.tagsBucket as TestBucket<Tag>
+        tagsBucket.clear()
+    }
+
     protected fun getResourceString(id: Int): String? {
         val targetContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
         return targetContext.resources.getString(id)
+    }
+
+    protected fun getRandomString(len: Int): String {
+        val random = SecureRandom()
+        val bytes = ByteArray(len)
+        random.nextBytes(bytes)
+
+        return (bytes.indices)
+                .map {
+                    charPool[random.nextInt(charPool.size)]
+                }.joinToString("")
+    }
+
+    protected fun createTag(tagName: String) {
+        TagUtils.createTagIfMissing(tagsBucket, tagName)
     }
 }
--- Simplenote/src/androidTest/java/com/automattic/simplenote/integration/AddTagActivityTest.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/integration/AddTagActivityTest.kt
@@ -1,43 +1,27 @@
 package com.automattic.simplenote.integration
 
-import android.content.Context
+import android.app.Activity
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.contrib.ActivityResultMatchers.hasResultCode
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.MediumTest
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
 import com.automattic.simplenote.AddTagActivity
 import com.automattic.simplenote.BaseUITest
 import com.automattic.simplenote.R
-import com.automattic.simplenote.SimplenoteTest
-import com.automattic.simplenote.models.Tag
-import com.automattic.simplenote.utils.TagUtils
-import com.automattic.simplenote.utils.TestBucket
 import com.automattic.simplenote.utils.hasTextInputLayoutErrorText
 import org.hamcrest.CoreMatchers.not
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
 class AddActivityTest : BaseUITest() {
-    private lateinit var application: SimplenoteTest
-    private lateinit var tagsBucket: TestBucket<Tag>
-
-    @Before
-    fun setup() {
-        application = getApplicationContext() as SimplenoteTest
-        tagsBucket = application.tagsBucket as TestBucket<Tag>
-        tagsBucket.clear()
-    }
 
     @Test
     fun addNewValidTag() {
@@ -54,7 +38,7 @@ class AddActivityTest : BaseUITest() {
 
     @Test
     fun addTagAlreadyExists() {
-        TagUtils.createTagIfMissing(tagsBucket, "tag1")
+        createTag("tag1")
         Assert.assertEquals(tagsBucket.count(), 1)
 
         ActivityScenario.launch(AddTagActivity::class.java)
@@ -70,18 +54,83 @@ class AddActivityTest : BaseUITest() {
 
     @Test
     fun addTagTooLong() {
-        TagUtils.createTagIfMissing(tagsBucket, "tag1")
-        Assert.assertEquals(tagsBucket.count(), 1)
-
         ActivityScenario.launch(AddTagActivity::class.java)
 
-        val newTag =
-        onView(withId(R.id.tag_input)).perform(replaceText("tag1"))
+        val newTag = getRandomString(257)
+        onView(withId(R.id.tag_input)).perform(replaceText(newTag))
         onView(withId(R.id.button_positive)).check(matches(not(isEnabled())))
 
-        val existsMessage = getResourceString(R.string.tag_error_exists) ?: ""
-        onView(withId(R.id.tag_layout)).check(matches(hasTextInputLayoutErrorText(existsMessage)))
+        val tooLongMessage = getResourceString(R.string.tag_error_length) ?: ""
+        onView(withId(R.id.tag_layout)).check(matches(hasTextInputLayoutErrorText(tooLongMessage)))
 
+        Assert.assertEquals(tagsBucket.count(), 0)
+    }
+
+    @Test
+    fun addTagWithSpace() {
+        Assert.assertEquals(tagsBucket.count(), 0)
+
+        val activityScenario = ActivityScenario.launch(AddTagActivity::class.java)
+
+        onView(withId(R.id.tag_input)).perform(replaceText("tag 1"))
+        onView(withId(R.id.button_positive)).check(matches(not(isEnabled())))
+
+        val spaceMessage = getResourceString(R.string.tag_error_spaces) ?: ""
+        onView(withId(R.id.tag_layout)).check(matches(hasTextInputLayoutErrorText(spaceMessage)))
+        onView(withId(R.id.button_negative)).perform(click())
+        assertThat(activityScenario.result, hasResultCode(Activity.RESULT_CANCELED))
+
+        Assert.assertEquals(tagsBucket.count(), 0)
+    }
+
+    @Test
+    fun addTagEmpty() {
+        Assert.assertEquals(tagsBucket.count(), 0)
+
+        val activityScenario = ActivityScenario.launch(AddTagActivity::class.java)
+
+        onView(withId(R.id.tag_input)).perform(replaceText("tag1"))
+        onView(withId(R.id.button_positive)).check(matches(isEnabled()))
+
+        onView(withId(R.id.tag_input)).perform(replaceText(""))
+        onView(withId(R.id.button_positive)).check(matches(not(isEnabled())))
+
+        val spaceMessage = getResourceString(R.string.tag_error_empty) ?: ""
+        onView(withId(R.id.tag_layout)).check(matches(hasTextInputLayoutErrorText(spaceMessage)))
+        onView(withId(R.id.button_negative)).perform(click())
+        assertThat(activityScenario.result, hasResultCode(Activity.RESULT_CANCELED))
+
+
+        Assert.assertEquals(tagsBucket.count(), 0)
+    }
+
+    @Test
+    fun addTagCancel() {
+        Assert.assertEquals(tagsBucket.count(), 0)
+
+        val activityScenario = ActivityScenario.launch(AddTagActivity::class.java)
+
+        onView(withId(R.id.tag_input)).perform(replaceText("tag1"))
+        onView(withId(R.id.button_positive)).check(matches(isEnabled()))
+        onView(withId(R.id.button_negative)).perform(click())
+
+        assertThat(activityScenario.result, hasResultCode(Activity.RESULT_CANCELED))
+
+        Assert.assertEquals(tagsBucket.count(), 0)
+    }
+
+    @Test
+    fun addSecondValidTag() {
+        createTag("tag5")
         Assert.assertEquals(tagsBucket.count(), 1)
+
+        val activityScenario = ActivityScenario.launch(AddTagActivity::class.java)
+
+        onView(withId(R.id.tag_input)).perform(replaceText("tag6"))
+        onView(withId(R.id.button_positive)).check(matches(isEnabled()))
+        onView(withId(R.id.button_positive)).perform(click())
+        assertThat(activityScenario.result, hasResultCode(Activity.RESULT_OK))
+
+        Assert.assertEquals(tagsBucket.count(), 2)
     }
 }
