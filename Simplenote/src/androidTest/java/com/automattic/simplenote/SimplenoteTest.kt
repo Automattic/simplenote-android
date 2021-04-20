1f737ad365ffc7a145558af9a8ac6b0d718733eb
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-19 21:31:20 -0500

    Add a parameter to SimplenoteTest class to decide whether to use the default bucket implementation or TestBucket one.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/SimplenoteTest.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/SimplenoteTest.kt
@@ -6,6 +6,9 @@ import com.automattic.simplenote.utils.TestBucket
 import com.simperium.client.Bucket
 
 class SimplenoteTest : Simplenote() {
+    // Decides whether use the default bucket implementation or our own TestBucket
+    var useDefaultBucketImpl = true
+
     private val tagsBucket = object : TestBucket<Tag>("tags") {
         override fun build(key: String?): Tag {
             return Tag(key)
@@ -19,10 +22,18 @@ class SimplenoteTest : Simplenote() {
     }
 
     override fun getTagsBucket(): Bucket<Tag> {
+        if (useDefaultBucketImpl) {
+            return super.getTagsBucket()
+        }
+
         return tagsBucket
     }
 
     override fun getNotesBucket(): Bucket<Note> {
+        if (useDefaultBucketImpl) {
+            return super.getNotesBucket()
+        }
+
         return notesBucket
     }
 }
