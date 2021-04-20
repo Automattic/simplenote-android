6634be5ecbd1c248a83a12ff3bba2b5adabc03d9
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-19 23:06:46 -0500

    Renamed AddActivityTest to AddTagActivityTest.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/integration/AddTagActivityTest.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/integration/AddTagActivityTest.kt
@@ -21,7 +21,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
-class AddActivityTest : BaseUITest() {
+class AddTagActivityTest : BaseUITest() {
 
     @Test
     fun addNewValidTag() {
