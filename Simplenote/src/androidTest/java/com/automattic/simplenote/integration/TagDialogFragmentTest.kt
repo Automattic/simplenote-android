d4a84d649a9f37769686da51db4c78d41343eed6
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-21 21:21:36 -0500

    Added TagDialogFragmentTest class for integration tests of TagDialogFragment.

---
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/integration/TagDialogFragmentTest.kt
@@ -0,0 +1,12 @@
+package com.automattic.simplenote.integration
+
+import androidx.test.filters.MediumTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.automattic.simplenote.BaseUITest
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+@MediumTest
+class TagDialogFragmentTest : BaseUITest() {
+
+}
