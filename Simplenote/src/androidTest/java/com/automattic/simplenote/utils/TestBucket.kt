9576142779212646dc0ba4adcc41c429ac5bd2cd
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-22 19:27:37 -0500

    Added TestObjectCursor to implement allObjects in TestBucket.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/utils/TestBucket.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/utils/TestBucket.kt
@@ -1,5 +1,7 @@
 package com.automattic.simplenote.utils
 
+import android.database.AbstractCursor
+import android.database.CharArrayBuffer
 import com.simperium.client.Bucket
 import com.simperium.client.BucketObjectMissingException
 import com.simperium.client.Syncable
@@ -39,7 +41,81 @@ abstract class TestBucket<T : Syncable>(name: String) : Bucket<T>(null, name, nu
         }
     }
 
+    override fun allObjects(): ObjectCursor<T> {
+        return TestObjectCursor(objects)
+    }
+
+    override fun reset() {
+        clear()
+    }
+
     fun clear() {
         objects.clear()
     }
+}
+
+class TestObjectCursor<T : Syncable>(private val objects: MutableList<T>) : AbstractCursor(), Bucket.ObjectCursor<T> {
+    private val columns = arrayOf("simperiumKey", "object")
+
+    override fun getCount(): Int {
+        return objects.size
+    }
+
+
+    override fun getColumnNames(): Array<String> {
+        return columns
+    }
+
+    override fun getColumnCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBlob(columnIndex: Int): ByteArray {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getString(columnIndex: Int): String {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun copyStringToBuffer(columnIndex: Int, buffer: CharArrayBuffer?) {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getShort(columnIndex: Int): Short {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getInt(columnIndex: Int): Int {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getLong(columnIndex: Int): Long {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getFloat(columnIndex: Int): Float {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getDouble(columnIndex: Int): Double {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getType(columnIndex: Int): Int {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun isNull(column: Int): Boolean {
+        throw RuntimeException("not implemented")
+    }
+
+    override fun getSimperiumKey(): String {
+        return objects[position].simperiumKey
+    }
+
+    override fun getObject(): T {
+        return objects[position]
+    }
+
 }
