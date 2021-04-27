e375fd9f4f77881972567c4dc6fd02189b9bc86d
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-27 04:42:41 -0500

    Added Test 6 (Delete Tags) for TagsActivity.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/integration/TagsActivityTest.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/integration/TagsActivityTest.kt
@@ -5,6 +5,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.MediumTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -133,5 +134,87 @@ class TagsActivityTest : BaseUITest() {
 
         val renameTagTitle = getResourceString(R.string.rename_tag)
         onView(withText(renameTagTitle)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun deleteTagsShowRemoveTagsFromNotes() {
+        createTag("other")
+        createTag("tag1")
+        createTag("tag2")
+        createTag("tag3")
+        // To edit tags, tags should belong a note
+        val note1 = createNote("Hello1", listOf("tag1", "tag2"))
+        val note2 = createNote("Hello2", listOf("tag2"))
+        val note3 = createNote("Hello3", listOf("tag1", "tag2", "tag3"))
+
+        Assert.assertEquals(tagsBucket.count(), 4)
+        Assert.assertEquals(notesBucket.count(), 3)
+
+        ActivityScenario.launch(TagsActivity::class.java)
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(0, R.id.tag_name))
+                .check(matches(withText("other")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(0, R.id.tag_count))
+                .check(matches(withText("")))
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(1, R.id.tag_name))
+                .check(matches(withText("tag1")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(1, R.id.tag_count))
+                .check(matches(withText("2")))
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(2, R.id.tag_name))
+                .check(matches(withText("tag2")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(2, R.id.tag_count))
+                .check(matches(withText("3")))
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(3, R.id.tag_name))
+                .check(matches(withText("tag3")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(3, R.id.tag_count))
+                .check(matches(withText("1")))
+
+        // Delete tag other
+        onView(withRecyclerView(R.id.list).atPositionOnView(0, R.id.tag_trash))
+                .perform(click())
+
+        Assert.assertEquals(tagsBucket.count(), 3)
+        Assert.assertEquals(notesBucket.count(), 3)
+        Assert.assertEquals(note1.tags, listOf("tag1", "tag2"))
+        Assert.assertEquals(note2.tags, listOf("tag2"))
+        Assert.assertEquals(note3.tags, listOf("tag1", "tag2", "tag3"))
+
+        // Try Delete tag tag2
+        onView(withRecyclerView(R.id.list).atPositionOnView(1, R.id.tag_trash))
+                .perform(click())
+
+        val deleteTagTitle = getResourceString(R.string.delete_tag)
+        val confirmDeleteTagMessage = getResourceString(R.string.confirm_delete_tag)
+        onView(withText(deleteTagTitle)).check(matches(isDisplayed()))
+        onView(withText(confirmDeleteTagMessage)).check(matches(isDisplayed()))
+        val noText = getResourceString(R.string.no)
+        onView(withText(noText)).inRoot(RootMatchers.isDialog()).perform(click())
+
+        Assert.assertEquals(tagsBucket.count(), 3)
+        Assert.assertEquals(notesBucket.count(), 3)
+        Assert.assertEquals(note1.tags, listOf("tag1", "tag2"))
+        Assert.assertEquals(note2.tags, listOf("tag2"))
+        Assert.assertEquals(note3.tags, listOf("tag1", "tag2", "tag3"))
+
+        // Delete tag tag2
+        onView(withRecyclerView(R.id.list).atPositionOnView(1, R.id.tag_trash))
+                .perform(click())
+
+        onView(withText(deleteTagTitle)).check(matches(isDisplayed()))
+        onView(withText(confirmDeleteTagMessage)).check(matches(isDisplayed()))
+        val yesText = getResourceString(R.string.yes)
+        onView(withText(yesText)).inRoot(RootMatchers.isDialog()).perform(click())
+
+        // Avoid flaky tests with AsyncTask
+        Thread.sleep(1000)
+
+        Assert.assertEquals(tagsBucket.count(), 2)
+        Assert.assertEquals(notesBucket.count(), 3)
+        Assert.assertEquals(note1.tags, listOf("tag1"))
+        Assert.assertEquals(note2.tags, listOf<String>())
+        Assert.assertEquals(note3.tags, listOf("tag1", "tag3"))
     }
 }
