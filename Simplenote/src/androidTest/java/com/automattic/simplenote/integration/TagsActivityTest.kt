582f131d9abd1d91dd3c7614dc110dacba52a528
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-27 03:18:50 -0500

    Added Test 1 (List of Tags should be complete and show the number of notes where the Tag was assigned) for TagsActivity.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/integration/TagsActivityTest.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/integration/TagsActivityTest.kt
@@ -1,7 +1,57 @@
 package com.automattic.simplenote.integration
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.filters.MediumTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.automattic.simplenote.BaseUITest
+import com.automattic.simplenote.R
+import com.automattic.simplenote.TagsActivity
+import com.automattic.simplenote.utils.withRecyclerView
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4ClassRunner::class)
+@MediumTest
 class TagsActivityTest : BaseUITest() {
 
+    @Test
+    fun listTagsShouldShowCompleteListTags() {
+        createTag("tag1")
+        createTag("tag2")
+        createTag("tag3")
+        createTag("other")
+        // To edit tags, tags should belong a note
+        createNote("Hello1", listOf("tag1", "tag2"))
+        createNote("Hello2", listOf("tag2"))
+        createNote("Hello3", listOf("tag1", "tag2", "tag3"))
+
+        Assert.assertEquals(tagsBucket.count(), 4)
+        Assert.assertEquals(notesBucket.count(), 3)
+
+        ActivityScenario.launch(TagsActivity::class.java)
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(0, R.id.tag_name))
+                .check(matches(withText("tag1")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(0, R.id.tag_count))
+                .check(matches(withText("2")))
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(1, R.id.tag_name))
+                .check(matches(withText("tag2")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(1, R.id.tag_count))
+                .check(matches(withText("3")))
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(2, R.id.tag_name))
+                .check(matches(withText("tag3")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(2, R.id.tag_count))
+                .check(matches(withText("1")))
+
+        onView(withRecyclerView(R.id.list).atPositionOnView(3, R.id.tag_name))
+                .check(matches(withText("other")))
+        onView(withRecyclerView(R.id.list).atPositionOnView(3, R.id.tag_count))
+                .check(matches(withText("")))
+    }
 }
