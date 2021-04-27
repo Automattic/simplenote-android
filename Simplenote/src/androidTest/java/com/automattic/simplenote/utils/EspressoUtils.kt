51a5775336208ef72e8cad31201ddafb8a42a1e8
Danilo Dominguez <danilo@accionasolutions.com>
2021-04-27 02:59:15 -0500

    Added helper functions for testing ReceyclerViews.

--- Simplenote/src/androidTest/java/com/automattic/simplenote/utils/EspressoUtils.kt
+++ Simplenote/src/androidTest/java/com/automattic/simplenote/utils/EspressoUtils.kt
@@ -1,10 +1,13 @@
 package com.automattic.simplenote.utils
 
+import android.content.res.Resources
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputLayout
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
+
 
 fun hasTextInputLayoutErrorText(expectedErrorText: String): Matcher<View> {
     return object : TypeSafeMatcher<View>() {
@@ -24,3 +27,51 @@ fun hasTextInputLayoutErrorText(expectedErrorText: String): Matcher<View> {
         }
     }
 }
+
+class RecyclerViewMatcher(private val recyclerViewId: Int) {
+    fun atPositionOnView(position: Int, targetViewId: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            var resources: Resources? = null
+            var childView: View? = null
+
+            override fun describeTo(description: Description) {
+                var idDescription = recyclerViewId.toString()
+                if (this.resources != null) {
+                    idDescription = try {
+                        this.resources!!.getResourceName(recyclerViewId)
+                    } catch (var4: Resources.NotFoundException) {
+                        String.format("%s (resource name not found)",
+                                Integer.valueOf(recyclerViewId)
+                        )
+                    }
+
+                }
+
+                description.appendText("with id: $idDescription")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+
+                this.resources = view.resources
+
+                if (childView == null) {
+                    val recyclerView = view.rootView.findViewById(recyclerViewId) as RecyclerView
+                    if (recyclerView.id == recyclerViewId) {
+                        childView = recyclerView.findViewHolderForAdapterPosition(position)!!.itemView
+                    } else {
+                        return false
+                    }
+                }
+
+                return if (targetViewId == -1) {
+                    view === childView
+                } else {
+                    val targetView = childView!!.findViewById<View>(targetViewId)
+                    view === targetView
+                }
+            }
+        }
+    }
+}
+
+fun withRecyclerView(recyclerViewId: Int): RecyclerViewMatcher = RecyclerViewMatcher(recyclerViewId)
