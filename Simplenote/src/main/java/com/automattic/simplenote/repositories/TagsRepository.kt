de122951b467e2eeaf8be93a1167e2dd1510887a
Danilo Dominguez <danilo@accionasolutions.com>
2021-05-13 04:30:00 -0500

    Added isTagConflict to TagRepository to handle conflicts with other tags.

--- Simplenote/src/main/java/com/automattic/simplenote/repositories/TagsRepository.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/repositories/TagsRepository.kt
@@ -6,6 +6,7 @@ interface TagsRepository {
     fun saveTag(tagName: String): Boolean
     fun isTagValid(tagName: String): Boolean
     fun isTagMissing(tagName: String): Boolean
+    fun isTagConflict(tagName: String, oldTagName: String): Boolean
     fun getCanonicalTagName(tagName: String): String
     fun renameTag(tagName: String, oldTag: Tag): Boolean
 }
