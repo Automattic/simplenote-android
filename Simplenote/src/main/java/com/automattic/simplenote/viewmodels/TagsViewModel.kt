f517a4ba5a3fe684dba5565e15c8d157bb1aa164
Danilo Dominguez <danilo@accionasolutions.com>
2021-05-14 03:05:15 -0500

    Added base TagsViewModel.

---
+++ Simplenote/src/main/java/com/automattic/simplenote/viewmodels/TagsViewModel.kt
@@ -0,0 +1,27 @@
+package com.automattic.simplenote.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.automattic.simplenote.models.Tag
+import com.automattic.simplenote.models.TagItem
+import com.automattic.simplenote.repositories.TagsRepository
+
+class TagsViewModel(private val tagsRepository: TagsRepository) : ViewModel() {
+    private val _uiState = MutableLiveData<UiState>()
+    val uiState: LiveData<UiState> = _uiState
+
+    private val _event = SingleLiveEvent<TagsEvent>()
+    val event: LiveData<TagsEvent> = _event
+    
+    data class UiState(private val tags: List<TagItem>, private val searchQuery: String)
+}
+
+sealed class TagsEvent {
+    object AddTagEvent : TagsEvent()
+    object LongAddTagEvent : TagsEvent()
+    object ShowSearch : TagsEvent()
+    object CloseSearch : TagsEvent()
+    object ResultEvent : TagsEvent()
+    data class EditTagEvent(val tag: Tag) : TagsEvent()
+}
