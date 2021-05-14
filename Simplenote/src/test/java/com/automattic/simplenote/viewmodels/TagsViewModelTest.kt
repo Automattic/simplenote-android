347014928c3e42e6591314d21d7e3563b34b19ca
Danilo Dominguez <danilo@accionasolutions.com>
2021-05-14 07:28:27 -0500

    Added tests for TagsViewModelTest.

--- Simplenote/src/test/java/com/automattic/simplenote/viewmodels/TagsViewModelTest.kt
+++ Simplenote/src/test/java/com/automattic/simplenote/viewmodels/TagsViewModelTest.kt
@@ -1,11 +1,13 @@
 package com.automattic.simplenote.viewmodels
 
+import android.view.View
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.automattic.simplenote.models.Tag
 import com.automattic.simplenote.models.TagItem
 import com.automattic.simplenote.repositories.TagsRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
@@ -13,7 +15,9 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.stub
 
 
@@ -88,6 +92,129 @@ class TagsViewModelTest {
 
         assertEquals(viewModel.uiState.value?.tagItems, tagItems)
         assertEquals(viewModel.uiState.value?.searchUpdate, true)
+        assertNull(viewModel.uiState.value?.searchQuery)
+    }
+
+    @Test
+    fun searchShouldFilterTags() = runBlockingTest {
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(tagItems)
+        }
+        fakeTagsRepository.stub {
+            onBlocking { tagsChanged() }.doReturn(emptyFlow())
+        }
+        viewModel.start()
+
+        val filteredList = listOf(tagItems[1])
+        val searchQuery = "tag2"
+        fakeTagsRepository.stub {
+            onBlocking { searchTags(any()) }.doReturn(filteredList)
+        }
+        viewModel.search(searchQuery)
+
+        assertEquals(viewModel.uiState.value?.tagItems, filteredList)
+        assertEquals(viewModel.uiState.value?.searchUpdate, true)
+        assertEquals(viewModel.uiState.value?.searchQuery, "tag2")
+    }
+
+    @Test
+    fun afterAddingATagUpdateOnResultShouldUpdateUiState() = runBlockingTest {
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(tagItems)
+        }
+        fakeTagsRepository.stub {
+            onBlocking { tagsChanged() }.doReturn(emptyFlow())
+        }
+        viewModel.start()
+
+        // Add a new tag
+        tagItems.add(TagItem(Tag("tag10"), 2))
+
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(tagItems)
+        }
+
+        viewModel.updateOnResult()
+
+        assertEquals(viewModel.uiState.value?.tagItems, tagItems)
+        assertEquals(viewModel.uiState.value?.searchUpdate, false)
+        assertNull(viewModel.uiState.value?.searchQuery)
+    }
+
+    @Test
+    fun clickEditTagShouldTriggerEventEditTagEvent() {
+
+        viewModel.clickEditTag(tagItems[0])
+
+        assertEquals(viewModel.event.value, TagsEvent.EditTagEvent(tagItems[0]))
+    }
+
+    @Test
+    fun clickDeleteTagOfTagWithNotesShouldTriggerDeleteTagEvent() {
+        viewModel.clickDeleteTag(tagItems[1])
+
+        assertEquals(viewModel.event.value, TagsEvent.DeleteTagEvent(tagItems[1]))
+    }
+
+    @Test
+    fun clickDeleteTagOfTagWithNotesShouldDeleteTagDirectly() = runBlockingTest {
+        fakeTagsRepository.stub {
+            onBlocking { deleteTag(any()) }.doReturn(Unit)
+        }
+
+        fakeTagsRepository.stub {
+            onBlocking { tagsChanged() }.doReturn(flow{ emit(true) })
+        }
+
+        val updatedTags = tagItems.filter { tagItem -> tagItem.tag.name != "tag2" }
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(updatedTags)
+        }
+
+        viewModel.clickDeleteTag(tagItems[1])
+
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(updatedTags)
+        }
+        viewModel.updateOnResult()
+
+        assertEquals(viewModel.uiState.value?.tagItems, updatedTags)
+        assertEquals(viewModel.uiState.value?.searchUpdate, false)
+        assertNull(viewModel.uiState.value?.searchQuery)
+    }
+
+    @Test
+    fun longClickDeleteTagShouldTriggerLongDeleteTagEvent() {
+        val view = mock<View>()
+        viewModel.longClickDeleteTag(view)
+
+        assertEquals(viewModel.event.value, TagsEvent.LongDeleteTagEvent(view))
+    }
+
+    @Test
+    fun deleteTagUpdatesListOfTags() {
+        fakeTagsRepository.stub {
+            onBlocking { deleteTag(any()) }.doReturn(Unit)
+        }
+
+        fakeTagsRepository.stub {
+            onBlocking { tagsChanged() }.doReturn(flow{ emit(true) })
+        }
+
+        val updatedTags = tagItems.filter { tagItem -> tagItem.tag.name != "tag3" }
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(updatedTags)
+        }
+
+        viewModel.deleteTag(tagItems[2])
+
+        fakeTagsRepository.stub {
+            onBlocking { allTags() }.doReturn(updatedTags)
+        }
+        viewModel.updateOnResult()
+
+        assertEquals(viewModel.uiState.value?.tagItems, updatedTags)
+        assertEquals(viewModel.uiState.value?.searchUpdate, false)
         assertNull(viewModel.uiState.value?.searchQuery)
     }
 }
