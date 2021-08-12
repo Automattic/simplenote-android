62a7094e1c7908896fbcf5a12ab78f5a743b25da
danilodominguezperez <danilo.dominguez@automattic.com>
2021-08-12 11:42:22 -0500

    Change setup views methods to be extensions of ActivityTagAddBinding
    Reduces the methods' line lengths since it does not have to include
    `binding.`.

--- Simplenote/src/main/java/com/automattic/simplenote/AddTagActivity.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/AddTagActivity.kt
@@ -6,7 +6,6 @@ import android.text.method.LinkMovementMethod
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
@@ -31,74 +30,73 @@ class AddTagActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityTagAddBinding = ActivityTagAddBinding.inflate(layoutInflater)
 
-        setObservers(binding)
-        setupLayout(binding)
-        setupViews(binding)
+        binding.setObservers(this)
+        binding.setupLayout(this)
+        binding.setupViews()
 
         viewModel.start()
 
         setContentView(binding.root)
     }
 
-    private fun setupViews(binding: ActivityTagAddBinding) {
-        binding.title.text = getString(R.string.add_tag)
-        binding.tagInput.doAfterTextChanged { s -> viewModel.updateUiState(s.toString()) }
-        binding.buttonNegative.setOnClickListener { viewModel.close() }
-        binding.buttonPositive.setOnClickListener { viewModel.saveTag() }
+    private fun ActivityTagAddBinding.setupViews() {
+        title.text = getString(R.string.add_tag)
+        tagInput.doAfterTextChanged { s -> viewModel.updateUiState(s.toString()) }
+        buttonNegative.setOnClickListener { viewModel.close() }
+        buttonPositive.setOnClickListener { viewModel.saveTag() }
     }
 
-    private fun setupLayout(binding: ActivityTagAddBinding) {
+    private fun ActivityTagAddBinding.setupLayout(activity: AddTagActivity) {
         val widthScreen = resources.displayMetrics.widthPixels
         val widthMargin =
             TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56f, resources.displayMetrics).toInt()
         val widthMaximum =
             TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 312f, resources.displayMetrics).toInt()
-        val layout: LinearLayout = binding.layout
         layout.layoutParams.width = widthMaximum.coerceAtMost(widthScreen - widthMargin)
         layout.requestLayout()
         (layout.parent as View).setOnClickListener { viewModel.close() }
         layout.setOnClickListener(null)
         MorphSetup.setSharedElementTransitions(
-            this,
+            activity,
             layout,
             resources.getDimensionPixelSize(R.dimen.corner_radius_dialog)
         )
     }
 
-    private fun setObservers(binding: ActivityTagAddBinding) {
+    private fun ActivityTagAddBinding.setObservers(activity: AddTagActivity) {
         // Observe changes in the UI state
-        viewModel.uiState.observe(this, { (_, errorMsg) ->
+        viewModel.uiState.observe(activity, { (_, errorMsg) ->
             // Validate if the current state has an error
             if (errorMsg != null) {
-                binding.tagLayout.error = getString(errorMsg)
-                binding.buttonPositive.isEnabled = false
+                tagLayout.error = getString(errorMsg)
+                buttonPositive.isEnabled = false
             } else {
                 // If there is not an error, enable save button
-                binding.tagLayout.error = null
-                binding.buttonPositive.isEnabled = true
+                tagLayout.error = null
+                buttonPositive.isEnabled = true
             }
         })
 
-        viewModel.event.observe(this, { event: AddTagViewModel.Event? ->
+        viewModel.event.observe(activity, { event: AddTagViewModel.Event? ->
             when (event) {
                 AddTagViewModel.Event.START -> {
-                    binding.buttonPositive.isEnabled = false
+                    buttonPositive.isEnabled = false
                     Handler().postDelayed(
                         {
-                            binding.tagInput.requestFocus()
-                            DisplayUtils.showKeyboard(binding.tagInput)
+                            tagInput.requestFocus()
+                            DisplayUtils.showKeyboard(tagInput)
                         },
                         MorphCircleToRectangle.DURATION.toLong()
                     )
                 }
                 AddTagViewModel.Event.CLOSE -> finishAfterTransition()
                 AddTagViewModel.Event.FINISH -> {
-                    DisplayUtils.hideKeyboard(binding.tagInput)
+                    DisplayUtils.hideKeyboard(tagInput)
                     setResult(RESULT_OK)
                     finishAfterTransition()
                 }
                 AddTagViewModel.Event.SHOW_ERROR -> {
-                    DisplayUtils.hideKeyboard(binding.tagInput)
+                    DisplayUtils.hideKeyboard(tagInput)
                     showDialogError()
                 }
             }
@@ -106,7 +104,7 @@ class AddTagActivity : AppCompatActivity() {
     }
 
     private fun showDialogError() {
-        val dialog = AlertDialog.Builder(ContextThemeWrapper(this@AddTagActivity, R.style.Dialog))
+        val dialog = AlertDialog.Builder(ContextThemeWrapper(this, R.style.Dialog))
             .setTitle(R.string.error)
             .setMessage(
                 HtmlCompat.fromHtml(
@@ -116,7 +114,7 @@ class AddTagActivity : AppCompatActivity() {
                         "<span style=\"color:#",
                         Integer.toHexString(
                             ThemeUtils.getColorFromAttribute(
-                                this@AddTagActivity,
+                                this,
                                 R.attr.colorAccent
                             ) and 0xffffff
                         ),
