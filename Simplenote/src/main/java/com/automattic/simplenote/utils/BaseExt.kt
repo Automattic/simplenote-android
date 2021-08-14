1811c10fec21f7b66af805fe00e44f221159cabe
danilodominguezperez <danilo.dominguez@automattic.com>
2021-08-13 23:02:20 -0500

    Define simple toast function as extension function of Activity

--- Simplenote/src/main/java/com/automattic/simplenote/utils/BaseExt.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/utils/BaseExt.kt
@@ -1,8 +1,14 @@
 package com.automattic.simplenote.utils
 
+import android.widget.Toast
 import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+
+fun AppCompatActivity.toast(@StringRes resId: Int, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, getString(resId), length).show()
+}
 
 fun AppCompatActivity.getColorStr(@ColorRes color: Int): String {
     val hintColor = ContextCompat.getColor(this, color)
