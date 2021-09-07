0fc8aca5b383f5a59174c5c9083de4a682c33af3
danilodominguezperez <danilo.dominguez@automattic.com>
2021-09-07 16:29:14 -0500

    Add GetTagsUseCase to filter collaborators from tags
    GetTagsUseCase encapsulates the logic to get all the tags and the tags
    from a note. In case the domain logic changes, we just have to change
    this use case and it affects the whole application.

---
+++ Simplenote/src/test/java/com/automattic/simplenote/usescases/GetTagsUseCaseTest.kt
@@ -0,0 +1,82 @@
+package com.automattic.simplenote.usescases
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.automattic.simplenote.models.Note
+import com.automattic.simplenote.models.Tag
+import com.automattic.simplenote.models.TagItem
+import com.automattic.simplenote.repositories.CollaboratorsRepository
+import com.automattic.simplenote.repositories.SimperiumCollaboratorsRepository
+import com.automattic.simplenote.repositories.TagsRepository
+import com.automattic.simplenote.usecases.GetTagsUseCase
+import com.simperium.client.Bucket
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.stub
+
+@ExperimentalCoroutinesApi
+class GetTagsUseCaseTest {
+    @get:Rule val rule = InstantTaskExecutorRule()
+
+    private val tagsRepository: TagsRepository = mock(TagsRepository::class.java)
+    private val mockBucket: Bucket<*> = mock(Bucket::class.java)
+    private lateinit var collaboratorsRepository: CollaboratorsRepository
+
+    private lateinit var getTagsUseCase: GetTagsUseCase
+    private val tagItems = listOf(
+        TagItem(Tag("tag1"), 0),
+        TagItem(Tag("tag2"), 2),
+        TagItem(Tag("tag3"), 5),
+        TagItem(Tag("name@example.co.jp"), 2),
+        TagItem(Tag("name1@test.com"), 0),
+        TagItem(Tag("name@test"), 1),
+        TagItem(Tag("あいうえお@example.com"), 1),
+    )
+
+    @Before
+    fun setup() {
+        // Set mock bucket to avoid NPE
+        tagItems.forEach { it.tag.bucket = mockBucket }
+
+        collaboratorsRepository = SimperiumCollaboratorsRepository()
+        getTagsUseCase = GetTagsUseCase(tagsRepository, collaboratorsRepository)
+    }
+
+    @Test
+    fun allTagsShouldFilterCollaborators() = runBlockingTest {
+        tagsRepository.stub {
+            onBlocking { allTags() }.doReturn(tagItems)
+        }
+
+        val tagItemsExpected = listOf(
+            TagItem(Tag("tag1"), 0),
+            TagItem(Tag("tag2"), 2),
+            TagItem(Tag("tag3"), 5),
+            TagItem(Tag("name@test"), 1),
+            TagItem(Tag("あいうえお@example.com"), 1),
+        )
+        tagItemsExpected.forEach { it.tag.bucket = mockBucket }
+
+
+        val tagItemsResult = getTagsUseCase.allTags()
+
+        assertEquals(tagItemsExpected, tagItemsResult)
+    }
+
+    @Test
+    fun tagsForNoteShouldFilterCollaborators() {
+        val note = Note("key1")
+        note.content = "Hello World"
+        note.tags = listOf("tag1", "tag2", "name@example.co.jp", "name@test")
+
+        val expected = listOf("tag1", "tag2", "name@test")
+        val result = getTagsUseCase.getTags(note)
+
+        assertEquals(expected, result)
+    }
+}
