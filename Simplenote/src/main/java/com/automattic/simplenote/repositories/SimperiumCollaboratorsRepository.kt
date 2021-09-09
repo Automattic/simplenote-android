e3b5a75000c82fdcf16e257ce6d1be3e9dc700b7
danilodominguezperez <danilo.dominguez@automattic.com>
2021-09-09 11:21:45 -0500

    Add implementation for getCollaborators in CollaboratorsRepository

--- Simplenote/src/main/java/com/automattic/simplenote/repositories/SimperiumCollaboratorsRepository.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/repositories/SimperiumCollaboratorsRepository.kt
@@ -4,6 +4,7 @@ import android.text.TextUtils
 import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
 import com.automattic.simplenote.models.Note
 import com.simperium.client.Bucket
+import com.simperium.client.BucketObjectMissingException
 import javax.inject.Inject
 
 class SimperiumCollaboratorsRepository @Inject constructor(
@@ -19,7 +20,20 @@ class SimperiumCollaboratorsRepository @Inject constructor(
         return !TextUtils.isEmpty(collaborator) && EMAIL_ADDRESS.matcher(collaborator).matches()
     }
 
+    /**
+     * Return a list of collaborators (email addresses as tags) if the note for the given simperiumKey ([noteId]) is
+     * not in the trash and has not been deleted
+     */
     override fun getCollaborators(noteId: String): GetCollaboratorsResult {
-        TODO("Not yet implemented")
+        return try {
+            val note = notesBucket.get(noteId)
+            if (note.isDeleted) {
+                return GetCollaboratorsResult.NoteInTrash
+            }
+
+            return GetCollaboratorsResult.CollaboratorsList(note.tags.filter { tag -> isValidCollaborator(tag) })
+        } catch (e: BucketObjectMissingException) {
+            return GetCollaboratorsResult.NoteDeleted
+        }
     }
 }
