abe2a13faa019053a463b8ee39910d41dcd58923
danilodominguezperez <danilo.dominguez@automattic.com>
2021-09-09 13:25:23 -0500

    Add implementation of addCollaborator for CollaboratorsRepository

--- Simplenote/src/main/java/com/automattic/simplenote/repositories/SimperiumCollaboratorsRepository.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/repositories/SimperiumCollaboratorsRepository.kt
@@ -3,6 +3,7 @@ package com.automattic.simplenote.repositories
 import android.text.TextUtils
 import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
 import com.automattic.simplenote.models.Note
+import com.automattic.simplenote.utils.Either
 import com.simperium.client.Bucket
 import com.simperium.client.BucketObjectMissingException
 import javax.inject.Inject
@@ -25,19 +26,40 @@ class SimperiumCollaboratorsRepository @Inject constructor(
      * not in the trash and has not been deleted
      */
     override fun getCollaborators(noteId: String): CollaboratorsActionResult {
-        try {
-            val note = notesBucket.get(noteId)
-            if (note.isDeleted) {
-                return CollaboratorsActionResult.NoteInTrash
-            }
-
-            return CollaboratorsActionResult.CollaboratorsList(note.tags.filter { tag -> isValidCollaborator(tag) })
-        } catch (e: BucketObjectMissingException) {
-            return CollaboratorsActionResult.NoteDeleted
-        }
+       return when(val result = getNote(noteId)) {
+           is Either.Left -> result.l
+           is Either.Right ->
+               CollaboratorsActionResult.CollaboratorsList(filterCollaborators(result.r))
+       }
     }
 
     override fun addCollaborator(noteId: String, collaborator: String): CollaboratorsActionResult {
-        TODO("Not yet implemented")
+        if (!isValidCollaborator(collaborator)) {
+            return CollaboratorsActionResult.InvalidCollaborator
+        }
+
+        return when(val result = getNote(noteId)) {
+            is Either.Left -> result.l
+            is Either.Right -> {
+                val note = result.r
+                note.addTag(collaborator)
+                CollaboratorsActionResult.CollaboratorsList(filterCollaborators(note))
+            }
+        }
     }
+
+    private fun getNote(noteId: String): Either<CollaboratorsActionResult, Note> {
+        try {
+            val note = notesBucket.get(noteId)
+            if (note.isDeleted) {
+                return Either.Left(CollaboratorsActionResult.NoteInTrash)
+            }
+
+            return Either.Right(note)
+        } catch (e: BucketObjectMissingException) {
+            return Either.Left(CollaboratorsActionResult.NoteDeleted)
+        }
+    }
+
+    private fun filterCollaborators(note: Note) = note.tags.filter { tag -> isValidCollaborator(tag) }
 }
