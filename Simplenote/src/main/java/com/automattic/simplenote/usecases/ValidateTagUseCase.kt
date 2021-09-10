91e2a21d0f1d04f071c0dc1a9cab002bb9953136
danilodominguezperez <danilo.dominguez@automattic.com>
2021-09-10 11:52:34 -0500

    Refactor isTagValid to a more idiomatic Kotlin way

--- Simplenote/src/main/java/com/automattic/simplenote/usecases/ValidateTagUseCase.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/usecases/ValidateTagUseCase.kt
@@ -2,34 +2,20 @@ package com.automattic.simplenote.usecases
 
 import com.automattic.simplenote.repositories.CollaboratorsRepository
 import com.automattic.simplenote.repositories.TagsRepository
+import com.automattic.simplenote.utils.StrUtils.SPACE_STRING
 import javax.inject.Inject
 
 class ValidateTagUseCase @Inject constructor(
     private val tagsRepository: TagsRepository,
     private val collaboratorsRepository: CollaboratorsRepository) {
 
-    fun isTagValid(tagName: String): TagValidationResult {
-        if (tagName.isEmpty()) {
-            return TagValidationResult.TagEmpty
-        }
-
-        if (tagName.contains(" ")) {
-            return TagValidationResult.TagWithSpaces
-        }
-
-        if (collaboratorsRepository.isValidCollaborator(tagName)) {
-            return TagValidationResult.TagIsCollaborator
-        }
-
-        if (!tagsRepository.isTagValid(tagName)) {
-            return TagValidationResult.TagTooLong
-        }
-
-        if (!tagsRepository.isTagMissing(tagName)) {
-            return TagValidationResult.TagExists
-        }
-
-        return TagValidationResult.TagValid
+    fun isTagValid(tagName: String) = when {
+        tagName.isEmpty() -> TagValidationResult.TagEmpty
+        tagName.contains(SPACE_STRING) -> TagValidationResult.TagWithSpaces
+        collaboratorsRepository.isValidCollaborator(tagName) -> TagValidationResult.TagIsCollaborator
+        !tagsRepository.isTagValid(tagName) -> TagValidationResult.TagTooLong
+        !tagsRepository.isTagMissing(tagName) -> TagValidationResult.TagExists
+        else -> TagValidationResult.TagValid
     }
 
     sealed class TagValidationResult {
