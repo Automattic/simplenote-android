cbcb02e6564a4dca9ff00c4445c43fa32ca43d8a
danilodominguezperez <danilo.dominguez@automattic.com>
2021-09-27 15:15:20 -0500

    Add SessionManager to provides the current user (auth or unauthorized)

--- Simplenote/src/main/java/com/automattic/simplenote/di/DataModule.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/di/DataModule.kt
@@ -1,6 +1,5 @@
 package com.automattic.simplenote.di
 
-import android.content.Context
 import com.automattic.simplenote.Simplenote
 import com.automattic.simplenote.models.Note
 import com.automattic.simplenote.models.Tag
@@ -8,14 +7,16 @@ import com.automattic.simplenote.repositories.CollaboratorsRepository
 import com.automattic.simplenote.repositories.SimperiumCollaboratorsRepository
 import com.automattic.simplenote.repositories.SimperiumTagsRepository
 import com.automattic.simplenote.repositories.TagsRepository
+import com.simperium.Simperium
 import com.simperium.client.Bucket
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataModule {
@@ -25,6 +26,9 @@ abstract class DataModule {
 
         @Provides
         fun providesNotesBucket(simplenote: Simplenote): Bucket<Note> = simplenote.notesBucket
+
+        @Provides
+        fun providesSimperium(simplenote: Simplenote): Simperium = simplenote.simperium
     }
 
     @Binds
