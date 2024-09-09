8ab543e0b87bcb051f1a2d6d277483a4257e26dc
Gio Lodi <gio@mokacoding.com>
2024-09-09 14:04:27 +1000

    Update code freeze logic with milestone moving automation

--- fastlane/lanes/release.rb
+++ fastlane/lanes/release.rb
@@ -12,10 +12,11 @@ platform :android do
     new_version_with_beta = release_version_for_code_freeze
     new_version_final = release_version_next
     new_build_code = build_code_next
+    computed_release_branch_name = release_branch_name(release_version: new_version_final)
 
     message = <<~MESSAGE
       Code Freeze:
-      - New release branch from #{DEFAULT_BRANCH}: #{release_branch_name(release_version: new_version_final)}
+      - New release branch from #{DEFAULT_BRANCH}: #{computed_release_branch_name}
       - Current release version and build code: #{release_version_current} (#{build_code_current}).
       - New beta version and build code: #{new_version_with_beta} (#{new_build_code}).
     MESSAGE
@@ -27,7 +28,7 @@ platform :android do
     end
 
     UI.message 'Creating release branch...'
-    Fastlane::Helper::GitHelper.create_branch(release_branch_name(release_version: new_version_final), from: DEFAULT_BRANCH)
+    Fastlane::Helper::GitHelper.create_branch(computed_release_branch_name)
     UI.success("Done! New release branch is: #{git_branch}.")
 
     UI.message 'Bumping beta version and build code...'
@@ -50,7 +51,7 @@ platform :android do
 
     update_strings_for_translation_automation
 
-    unless skip_confirm || UI.confirm("Ready to push the new #{release_branch_name} branch the remote and let the automation configure branch protection and milestone on GitHub?")
+    unless skip_confirm || UI.confirm("Push the new #{computed_release_branch_name} branch the remote and let the automation configure branch protection and milestone on GitHub?")
       UI.user_error!("Terminating as requested. Don't forget to run the remainder of this automation manually.")
       next
     end
@@ -63,12 +64,88 @@ platform :android do
     copy_branch_protection(
       repository: GITHUB_REPO,
       from_branch: DEFAULT_BRANCH,
-      to_branch: release_branch_name
+      to_branch: computed_release_branch_name
     )
-    set_milestone_frozen_marker(
-      repository: GITHUB_REPO,
-      milestone: new_version_final,
-      freeze: true
+
+    freeze_milestone_and_move_assigned_prs_to_next_milestone(
+      milestone_to_freeze: new_version_final,
+      next_milestone: release_version_next
     )
+
+    next unless is_ci
+
+    message = <<~MESSAGE
+      Code freeze started successfully.
+
+      Next steps:
+
+      - Checkout `#{release_branch_name}` branch locally
+      - Update the simperium dependency to a stable version if needed
+      - Update the release notes that were extracted from RELEASE-NOTES.txt if appropriate
+      - Finalize the code freeze
+    MESSAGE
+    buildkite_annotate(context: 'code-freeze-success', style: 'success', message: message)
   end
+end
+
+def freeze_milestone_and_move_assigned_prs_to_next_milestone(
+  milestone_to_freeze:,
+  next_milestone:,
+  github_repository: GITHUB_REPO
+)
+  # Notice that the order of execution is important here and should not be changed.
+  #
+  # First, we move the PR from milestone_to_freeze to next_milestone.
+  # Then, we update milestone_to_freeze's tile with the frozen marker (traditionally ❄️ )
+  #
+  # If the order were to be reversed, the PRs lookup for milestone_to_freeze would yeld no value.
+  # That's because the lookup uses the milestone title, which would no longer be milestone_to_freeze, but milestone_to_freeze + the frozen marker.
+  begin
+    # Move PRs to next milestone
+    moved_prs = update_assigned_milestone(
+      repository: github_repository,
+      from_milestone: milestone_to_freeze,
+      to_milestone: next_milestone,
+      comment: "Version `#{milestone_to_freeze}` has entered code-freeze. The milestone of this PR has been updated to `#{next_milestone}`."
+    )
+
+    # Add ❄️ marker to milestone title to indicate we entered code-freeze
+    set_milestone_frozen_marker(
+      repository: github_repository,
+      milestone: milestone_to_freeze
+    )
+  rescue StandardError => e
+    moved_prs = []
+
+    report_milestone_error(error_title: "Error during milestone `#{milestone_to_freeze}` freezing and PRs milestone updating process: #{e.message}")
+  end
+
+  UI.message("Moved the following PRs to milestone #{next_milestone}: #{moved_prs.join(', ')}")
+
+  return unless is_ci
+
+  moved_prs_info = if moved_prs.empty?
+                     "No open PRs were targeting `#{milestone_to_freeze}` at the time of code-freeze."
+                   else
+                     "#{moved_prs.count} PRs targeting `#{milestone_to_freeze}` were still open at the time of code-freeze. They have been moved to `#{next_milestone}`:\n" \
+                       + moved_prs.map { |pr_num| "[##{pr_num}](https://github.com/#{GITHUB_REPO}/pull/#{pr_num})" }.join(', ')
+                   end
+
+  buildkite_annotate(
+    style: moved_prs.empty? ? 'success' : 'warning',
+    context: 'code-freeze-milestone-updates',
+    message: moved_prs_info
+  )
+end
+
+def report_milestone_error(error_title:)
+  error_message = <<-MESSAGE
+    #{error_title}
+    - If this is not the first time you are running the release task (e.g. retrying because it failed on first attempt), the milestone might have already been closed and this error is expected.
+    - Otherwise, please investigate the error.
+  MESSAGE
+
+  UI.error(error_message)
+
+  buildkite_annotate(style: 'warning', context: 'error-with-milestone', message: error_message) if is_ci
 end
