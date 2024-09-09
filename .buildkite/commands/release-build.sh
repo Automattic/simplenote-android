506bbd68daa1294db78537a2074d6a4caa3be48b
Gio Lodi <gio@mokacoding.com>
2024-09-09 15:51:59 +1000

    Use `find` instead of `ls -1` to find APK

--- .buildkite/commands/release-build.sh
+++ .buildkite/commands/release-build.sh
@@ -12,7 +12,7 @@ bundle exec fastlane build_for_distribution
 echo "--- :computer: Use deterministic APK name for next steps"
 
 OUTPUT_DIR="build"
-if [ $(ls -1 "$OUTPUT_DIR/*.apk" | wc -l) -gt 1 ]; then
+if [ "$(find "$OUTPUT_DIR/*.apk" | wc -l)" -gt 1 ]; then
   echo "Found more than one APK in $OUTPUT_DIR."
   exit 1
 fi
@@ -20,5 +20,5 @@ fi
 ORIGINAL_APK_PATH=$(ls -1 "$OUTPUT_DIR/*.apk")
 
 set -x
-mv $ORIGINAL_APK_PATH $OUTPUT_DIR/simplenote.apk
+mv "$ORIGINAL_APK_PATH" $OUTPUT_DIR/simplenote.apk
 set +x
